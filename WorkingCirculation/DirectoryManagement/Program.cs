c80386503d073b0dc5d2e75d717c40808ecc5876
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-04-19 12:45:36 +0200

    do docker stops received via args in co li (#12)

--- WorkingCirculation/DirectoryManagement/Program.cs
+++ WorkingCirculation/DirectoryManagement/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+void RunCommand(string commandToExecute, string workingDirectory) {
+    Process process = new Process();
+    process.StartInfo.WorkingDirectory = workingDirectory;
+    process.StartInfo.FileName = "cmd.exe";
+    process.StartInfo.Arguments = $"/c {commandToExecute}";
+    process.StartInfo.RedirectStandardOutput= true;
+    process.Start();
+    process.WaitForExit();
+    string output = process.StandardOutput.ReadToEnd();
+    Console.WriteLine(output);
+}
+
+string workingDirectory = Environment.GetCommandLineArgs()[1];
+string commandToExecute = Environment.GetCommandLineArgs()[2];
+
+RunCommand(commandToExecute, workingDirectory);
