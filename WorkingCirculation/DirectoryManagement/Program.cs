461a2c8ed2bda418968c1e7f99b6bba5554ad06e
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-04-22 10:57:23 +0200

    option to choose  between coli or explo for opening direCs

--- WorkingCirculation/DirectoryManagement/Program.cs
+++ WorkingCirculation/DirectoryManagement/Program.cs
@@ -1,6 +1,15 @@
 ﻿using DirectoryManagement;
 
+string choice = "2";
 string workingDirectory = Environment.GetCommandLineArgs()[1];
-string commandToExecute = Environment.GetCommandLineArgs()[2];
 
-DirectoryOperations.OpenDirectoryThroughCommandLine(commandToExecute, workingDirectory);
+if(choice == "1") 
+{
+    string commandToExecute = Environment.GetCommandLineArgs()[2];
+    DirectoryOperations.OpenDirectoryThroughCommandLine(commandToExecute, workingDirectory);
+}
+
+if(choice == "2") 
+{
+    DirectoryOperations.OpenDirectoryThroughExplorer(workingDirectory);
+}
