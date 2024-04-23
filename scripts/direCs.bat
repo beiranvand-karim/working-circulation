577f6349eb2982d040ee2375a57422f691929fe6
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-04-23 16:39:43 +0200

    open direCs by clicking a batch file (#19)

---
+++ scripts/direCs.bat
@@ -0,0 +1,7 @@
+@echo off
+
+C:/Users/....../source/repos/working-circulation/WorkingCirculation/DirectoryManagement/bin/Debug/net8.0/DirectoryManagement.exe "C:/feature naming/...../fend"
+C:/Users/....../source/repos/working-circulation/WorkingCirculation/DirectoryManagement/bin/Debug/net8.0/DirectoryManagement.exe "C:/feature naming/...../fend/fend...."
+C:/Users/....../source/repos/working-circulation/WorkingCirculation/DirectoryManagement/bin/Debug/net8.0/DirectoryManagement.exe "C:/feature naming/...../fend/fend...."
+C:/Users/....../source/repos/working-circulation/WorkingCirculation/DirectoryManagement/bin/Debug/net8.0/DirectoryManagement.exe "C:/feature naming/...../calls"
+C:/Users/....../source/repos/working-circulation/WorkingCirculation/DirectoryManagement/bin/Debug/net8.0/DirectoryManagement.exe "C:/feature naming/...../bend"
