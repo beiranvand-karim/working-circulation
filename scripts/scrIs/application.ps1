b35e8f648d42c6f536f285077c367f40e9942c12
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-04-24 12:40:14 +0200

    app and app env files

---
+++ scripts/scrIs/application.ps1
@@ -0,0 +1,9 @@
+get-content application.env | foreach {
+    $name, $value = $_.split("=")
+    set-content env:\$name $value
+}
+
+& $env:DOCKER_CLI_LOCATION -SwitchWindowsEngine
+set-location $env:COMPOSE_FILE_LOCATION
+docker compose pull
+docker compose up -d
