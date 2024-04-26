0bf72c5c8484639df2bdf9f3ab3eb6be2898dc5d
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-04-26 21:27:54 +0200

    rider guest application (#27)

---
+++ scripts/scrIs/rider guest application.ps1
@@ -0,0 +1,8 @@
+<#> fina = file name = rider guest application.ps1 #>
+
+get-content "rider guest application.env" | foreach {
+    $name, $value = $_.split("=")
+    set-content env:\$name $value
+}
+
+start-process -FilePath $env:RIDER_LOCATION  -ArgumentList $env:GUEST_APPLICATION_LOCATION
