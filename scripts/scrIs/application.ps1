a50126a64e63ba5f64e8513f03b9d59ce4155ed4
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-04-26 20:59:45 +0200

    use start process for application ps file as well

--- scripts/scrIs/application.ps1
+++ scripts/scrIs/application.ps1
@@ -5,7 +5,7 @@ get-content application.env | foreach {
     set-content env:\$name $value
 }
 
-& $env:DOCKER_CLI_LOCATION -SwitchWindowsEngine
+start-process -FilePath $env:DOCKER_CLI_LOCATION -ArgumentList  "-SwitchWindowsEngine"
 set-location $env:COMPOSE_FILE_LOCATION
 docker compose pull
 docker compose up -d
