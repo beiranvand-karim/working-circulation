8884549010092699b0e58352d308a97fbe56fd29
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-01 10:25:13 +0200

    receive latest changes of all projects

---
+++ scripts/scrIs/receive latest changes of all projects.ps1
@@ -0,0 +1,13 @@
+<#> fina = file name = receive latest changes of all projects.ps1 #>
+
+get-content "scrIs/receive latest changes of all projects.env" | ForEach-Object {
+    $name, $value = $_.split("=")
+    set-content env:\$name $value
+}
+
+start-process -FilePath $env:RECEIVER_LOCATION -ArgumentList  "checkout develop" -Wait
+start-process -FilePath $env:RECEIVER_LOCATION -ArgumentList  "pull" -Wait
+
+Write-Host -NoNewline  'press any key to coontinue ...'
+$null = $Host.UI.RawUI.ReadKey('NoEcho, IncludedKeyDown')
+
