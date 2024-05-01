b34ca6ef797c7fce7e7dc0ff0029a2cdcb597ff2
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-01 10:37:10 +0200

    stash cli co

---
+++ scripts/scrIs/stash.ps1
@@ -0,0 +1,9 @@
+<#> fina = file name = stash.ps1 #>
+
+get-content stash.env | ForEach-Object {
+    $name, $value = $_.split("=")
+    set-content env:\$name $value
+}
+
+set-location $env:LOCATION
+git stash --include-untracked
