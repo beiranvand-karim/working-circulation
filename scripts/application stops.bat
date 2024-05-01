c21660c6c229c121bb8c04b2939d0eb60a6ce130
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-01 17:21:53 +0200

    dockere compose stops bat file

---
+++ scripts/application stops.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM application stops
+REM application sttops.bat
+
+
+C:/Users/....../source/repos/working-circulation/WorkingCirculation/DockerComposeStops/bin/Debug/net8.0/DockerComposeStops.exe "C:/....." "docker compose stop ui cuttingdata"
