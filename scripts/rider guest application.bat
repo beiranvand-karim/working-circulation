0510347083bb2609d01ecfba4486d6ace6041468
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-01 17:26:32 +0200

    string fixes some

--- scripts/rider guest application.bat
+++ scripts/rider guest application.bat
@@ -1,6 +1,6 @@
 @echo off
 
 REM rider guest application
-REM applirider guest applicationcation.bat
+REM rider guest application.bat
 
 Powershell.exe -executionpolicy remotesigned -File "scrIs/rider guest application.ps1" 
