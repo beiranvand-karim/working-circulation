972f66fec9f8b3e39e2f1445babc2ac1c6e3ee47
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-04 19:06:39 +0200

    drop .example while copying (#55)

--- WorkingCirculation/EnvironmeentVariablesManagement/Program.cs
+++ WorkingCirculation/EnvironmeentVariablesManagement/Program.cs
@@ -7,16 +7,9 @@ string destinationDirectory = "/Users/karimbeiranvand/Documents/GitHub/working-c
 foreach (string file in Directory.EnumerateFiles(sourceDirectory))
 {
     string destPath =  destinationDirectory + '/' + Path.GetFileName(file);
-    File.Copy(file, destPath);
+    string[] words = destPath.Split('.');
+    string destFilePathName = words[0]  + '.' +  words[1];
+    File.Copy(file, destFilePathName);
 }
 
 //  todo add conds that t both direcs would exists
-
-
-string phrase = "/Users/karimbeiranvand/Documents/GitHub/working-circulation/scripts/environment-variables-files/application user interface.env.example";
-string[] words = phrase.Split('.');
-
-
-string destFilePathName = words[0]  + '.' +  words[1];
-
-Console.WriteLine(destFilePathName);
