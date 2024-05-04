429ad1243cb3983536ea8c0d4f9a25c5ef3f903b
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-04 18:38:20 +0200

    read file names from a directory

--- WorkingCirculation/EnvironmeentVariablesManagement/Program.cs
+++ WorkingCirculation/EnvironmeentVariablesManagement/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+string sourceDirectory = "/Users/karimbeiranvand/Documents/GitHub/working-circulation/scripts/environment-variables-example-files";
+
+string destinationDirectory = "/Users/karimbeiranvand/Documents/GitHub/working-circulation/scripts/environment-variables-files";
+
+foreach (string file in Directory.EnumerateFiles(sourceDirectory))
+{
+    string contents = File.ReadAllText(file);
+    Console.WriteLine(file);
+}
+
