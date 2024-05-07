8aab4f341b7bc7d3e3c1bb86b66d12a1024e9819
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-07 22:37:11 +0200

    move environment variables methods to different file (#64)

--- WorkingCirculation/EnvironmeentVariablesManagement/Program.cs
+++ WorkingCirculation/EnvironmeentVariablesManagement/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Program.cs
-using System.Text;
+using EnvironmentVariablesManagement;
 
 int choice=2;
 
@@ -10,87 +10,26 @@ if (choice == 1) {
 
     foreach (string file in Directory.EnumerateFiles(sourceDirectory))
     {
-        string destPath =  destinationDirectory + '/' + Path.GetFileName(file);
+        string destPath = destinationDirectory + '/' + Path.GetFileName(file);
         string[] words = destPath.Split('.');
-        string destFilePathName = words[0]  + '.' +  words[1];
+        string destFilePathName = words[0] + '.' + words[1];
         File.Copy(file, destFilePathName);
     }
 }
 
 //  todo add conds that t both direcs would exists
 
-static Dictionary<string, string> PairUpVariablesWithTheirValue(string fileNamePath, Dictionary<string, string> environmentVariablesSourceDictionary) {
-
-        Dictionary<string, string>  fileContentDictionaryToWriteToFile  =  [];
-
-        const Int32 BufferSize = 128;
-        using var fileStream = File.OpenRead(fileNamePath);
-        using var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize);
-        String line;
-
-        while ((line = streamReader.ReadLine()) != null) 
-        {
-            string[] brokenLine = line.Split("=");
-            string key = brokenLine[0];
-            string value = brokenLine[1];
-            string val;
-            environmentVariablesSourceDictionary.TryGetValue(key,  out val);
-            fileContentDictionaryToWriteToFile.Add(key, val);
-            Console.WriteLine(" dfkshfsf , ${0}  ,   ${1}",  key,  val);
-        }
-
-        return fileContentDictionaryToWriteToFile;
-
-}
-
-static Dictionary<string,  string> readKeyValueFromFile(string fileNamePath) {
-
-        Dictionary<string, string>  fileContentDictionary  =  [];
-
-        const Int32 BufferSize = 128;
-        using var fileStream = File.OpenRead(fileNamePath);
-        using var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize);
-        String line;
-
-        while ((line = streamReader.ReadLine()) != null) 
-        {
-            string[] brokenLine = line.Split("=");
-            string key = brokenLine[0];
-            string value = brokenLine[1];
-            fileContentDictionary.Add(key, value);
-        }
-        return fileContentDictionary;
-}
-
-static Dictionary<string, string> getAllEnvironmentVariablesAndValuesFromSourceFile(string environmentVariablesSourceDirectory)
-{
-    Dictionary<string, string>  keyValuePairs  = [];
-
-    foreach (string file in Directory.EnumerateFiles(environmentVariablesSourceDirectory))
-    {
-        keyValuePairs  =  readKeyValueFromFile(file);
-    }
-
-    return keyValuePairs;
-}
-
-
 if (choice == 2) 
 {
-    string environmentVariablesSourceDirectory  = "/Users/karimbeiranvand/Documents/GitHub/working-circulation/scripts/environment-variables-source";
+    string environmentVariablesSourceDirectory = "/Users/karimbeiranvand/Documents/GitHub/working-circulation/scripts/environment-variables-source";
 
-    Dictionary<string, string>  environmentVariablesSourceDictionary = getAllEnvironmentVariablesAndValuesFromSourceFile(environmentVariablesSourceDirectory);
-
-    foreach (KeyValuePair<string, string> kvp in environmentVariablesSourceDictionary)
-    {
-        Console.WriteLine("environmentVariablesSourceDictionary: Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-    }
+    Dictionary<string, string> environmentVariablesSourceDictionary = Something.GetAllEnvironmentVariablesAndValuesFromSourceFile(environmentVariablesSourceDirectory);
 
     string sourceDirectory = "/Users/karimbeiranvand/Documents/GitHub/working-circulation/scripts/environment-variables-example-files";
 
     foreach (string file in Directory.EnumerateFiles(sourceDirectory))  
     {
         Console.WriteLine(file);
-        Dictionary<string, string>  contentToWrite  =  PairUpVariablesWithTheirValue(file, environmentVariablesSourceDictionary);
+        Dictionary<string, string> contentToWrite = Something.PairUpVariablesWithTheirValue(file, environmentVariablesSourceDictionary);
     }
 }
