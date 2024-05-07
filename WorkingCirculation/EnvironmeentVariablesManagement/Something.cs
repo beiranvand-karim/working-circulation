4ebc57ed90cec0036c9fc265f387f88ed88837af
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-07 22:36:51 +0200

    move environment variables methods to different file

---
+++ WorkingCirculation/EnvironmeentVariablesManagement/Something.cs
@@ -0,0 +1,62 @@
+using System.Text;
+
+namespace EnvironmentVariablesManagement
+{
+    internal class Something 
+    {
+        public static Dictionary<string, string> PairUpVariablesWithTheirValue(string fileNamePath, Dictionary<string, string> environmentVariablesSourceDictionary) {
+
+            Dictionary<string, string>  fileContentDictionaryToWriteToFile = [];
+
+            const Int32 BufferSize = 128;
+            using var fileStream = File.OpenRead(fileNamePath);
+            using var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize);
+            String line;
+
+            while ((line = streamReader.ReadLine()) != null) 
+            {
+                string[] brokenLine = line.Split("=");
+                string key = brokenLine[0];
+                string value = brokenLine[1];
+                string val;
+                environmentVariablesSourceDictionary.TryGetValue(key, out val);
+                fileContentDictionaryToWriteToFile.Add(key, val);
+                Console.WriteLine("{0}={1}", key, val);
+            }
+
+            return fileContentDictionaryToWriteToFile;
+        }
+
+        public static Dictionary<string, string> ReadKeyValueFromFile(string fileNamePath) 
+        {
+
+            Dictionary<string, string>  fileContentDictionary  =  [];
+
+            const Int32 BufferSize = 128;
+            using var fileStream = File.OpenRead(fileNamePath);
+            using var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize);
+            String line;
+
+            while ((line = streamReader.ReadLine()) != null) 
+            {
+                string[] brokenLine = line.Split("=");
+                string key = brokenLine[0];
+                string value = brokenLine[1];
+                fileContentDictionary.Add(key, value);
+            }
+            return fileContentDictionary;
+        }
+
+        public static Dictionary<string, string> GetAllEnvironmentVariablesAndValuesFromSourceFile(string environmentVariablesSourceDirectory)
+        {
+            Dictionary<string, string>  keyValuePairs  = [];
+
+            foreach (string file in Directory.EnumerateFiles(environmentVariablesSourceDirectory))
+            {
+                keyValuePairs = Something.ReadKeyValueFromFile(file);
+            }
+
+            return keyValuePairs;
+        }
+    }
+}
