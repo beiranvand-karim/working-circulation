97a81bdc238d7c06165909466472632f1712ccc6
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-09 13:42:54 +0200

    entry points

---
+++ entry -points/macos-starting-pont.sh
@@ -0,0 +1,9 @@
+--templates-directory "environment-variables-example-files" \
+--destination-directory  "environment-variables-files" \
+--environment-variables-source-directory  "environment-variables-source"
+--feature-name "wonderful feeature"
+--executive-file-directory "/EnvironmeentVariablesManagement/bin/Debug/net8.0/EnvironmeentVariablesManagement"  \
+### origins: everytthing  else is build on  top of these two pillars
+--scripts-directory "/Users/karimbeiranvand/Documents/GitHub/working-circulation/scripts" \
+--repository-directory "/Users/karimbeiranvand/Documents/GitHub/working-circulation"
+--hosting-directory "/Users/karimbeiranvand"
