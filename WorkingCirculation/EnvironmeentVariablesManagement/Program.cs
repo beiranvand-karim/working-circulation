e0a46cc0c3c79d4c1cc769d579d0d2fd29edb5cb
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-09 20:00:46 +0200

    appsettings

--- WorkingCirculation/EnvironmeentVariablesManagement/Program.cs
+++ WorkingCirculation/EnvironmeentVariablesManagement/Program.cs
@@ -2,30 +2,43 @@
 using EnvironmentVariablesManagement;
 using Microsoft.Extensions.Configuration;
 
-string templatesDirectoryNameKey = "--templates-directory";
-string destinationDirectoryNameKey = "--destination-directory";
-string environmentVariablesSourceDirectoryNameKey = "--environment-variables-source-directory";
-string featureNameKey = "--feature-name";
-string executiveFileDirectoryNameKey = "--executive-file-directory";
-string scriptsDirectoryNameKey = "--scripts-directory";
 string repositoryDirectoryNameKey = "--repository-directory";
-string hostingDirectoryNameKey = "--hosting-directory";
 
-Console.WriteLine(Something.GetCommandLineArgByKey(templatesDirectoryNameKey));
-Console.WriteLine(Something.GetCommandLineArgByKey(destinationDirectoryNameKey));
-Console.WriteLine(Something.GetCommandLineArgByKey(environmentVariablesSourceDirectoryNameKey));
-Console.WriteLine(Something.GetCommandLineArgByKey(featureNameKey));
-Console.WriteLine(Something.GetCommandLineArgByKey(executiveFileDirectoryNameKey));
-Console.WriteLine(Something.GetCommandLineArgByKey(scriptsDirectoryNameKey));
-Console.WriteLine(Something.GetCommandLineArgByKey(repositoryDirectoryNameKey));
-Console.WriteLine(Something.GetCommandLineArgByKey(hostingDirectoryNameKey));
-
+string workingCirculationDirectoryName = Path.Combine(Something.GetCommandLineArgByKey(repositoryDirectoryNameKey), "WorkingCirculation");
+string environmeentVariablesManagementDirectoryName = Path.Combine(workingCirculationDirectoryName, "EnvironmeentVariablesManagement");
+string appsettings = Path.Combine(environmeentVariablesManagementDirectoryName, "appsettings.json");
 
 var builder = new ConfigurationBuilder();
 builder.SetBasePath(Directory.GetCurrentDirectory())
-       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+       .AddJsonFile(appsettings, optional: false, reloadOnChange: true);
  
 IConfiguration config = builder.Build();
+
+string templatesDirectoryNameKey = config["EnvironmentVariablesCommandLineArgumeentsNameKeys:TemplatesDirectoryNameKey"];
+string destinationDirectoryNameKey = config["EnvironmentVariablesCommandLineArgumeentsNameKeys:DestinationDirectoryNameKey"];
+string environmentVariablesSourceDirectoryNameKey = config["EnvironmentVariablesCommandLineArgumeentsNameKeys:EnvironmentVariablesSourceDirectoryNameKey"];
+string featureNameKey = config["EnvironmentVariablesCommandLineArgumeentsNameKeys:FeatureNameKey"];
+string executiveFileDirectoryNameKey = config["EnvironmentVariablesCommandLineArgumeentsNameKeys:ExecutiveFileDirectoryNameKey"];
+string scriptsDirectoryNameKey = config["EnvironmentVariablesCommandLineArgumeentsNameKeys:ScriptsDirectoryNameKey"];
+string hostingDirectoryNameKey = config["EnvironmentVariablesCommandLineArgumeentsNameKeys:HostingDirectoryNameKey"];
+
+string templatesDirectoryName =  Something.GetCommandLineArgByKey(templatesDirectoryNameKey);
+string destinationDirectoryName =  Something.GetCommandLineArgByKey(destinationDirectoryNameKey);
+string environmentVariablesSourceDirectoryName =  Something.GetCommandLineArgByKey(environmentVariablesSourceDirectoryNameKey);
+string featureName =  Something.GetCommandLineArgByKey(featureNameKey);
+string executiveFileDirectoryName =  Something.GetCommandLineArgByKey(executiveFileDirectoryNameKey);
+string scriptsDirectoryName =  Something.GetCommandLineArgByKey(scriptsDirectoryNameKey);
+string hostingDirectoryName =  Something.GetCommandLineArgByKey(hostingDirectoryNameKey);
+
+
+Console.WriteLine(templatesDirectoryName);
+Console.WriteLine(destinationDirectoryName);
+Console.WriteLine(environmentVariablesSourceDirectoryName);
+Console.WriteLine(featureName);
+Console.WriteLine(executiveFileDirectoryName);
+Console.WriteLine(scriptsDirectoryName);
+Console.WriteLine(hostingDirectoryName);
+
 
 int choice = 2;
 
