bb12f500a1a51b9aee592a3b1f64ae061e12217a
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-11 09:51:42 +0200

    get command line argument keys from appsettings.json (#87)

--- WorkingCirculation/EnvironmeentVariablesManagement/CommandLineArgs.cs
+++ WorkingCirculation/EnvironmeentVariablesManagement/CommandLineArgs.cs
@@ -1,8 +1,17 @@
 
+using Microsoft.Extensions.Configuration;
+
 namespace EnvironmentVariablesManagement
 {
     internal class CommandLineArgs 
     {
+        public static string GetKey(IConfiguration config,string key)
+        {
+            string groupKey = "EnvironmentVariablesCommandLineArgumentsNameKeys";
+            string commandLineArgumentKey = $""""{groupKey}:{key}"""";
+            return config[commandLineArgumentKey];
+        }
+        
         public static string GetByKey(string CommandLineArgKey)
         {
             var commandLineArgs = Environment.GetCommandLineArgs();
