a684c8080adab51d78055daa7f5ebb8eeeb89827
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-14 15:57:06 +0200

    555

---
+++ entry-points/winos-starting-pont.bat
@@ -0,0 +1,13 @@
+@echo off
+
+[ ... fill in here ...]/working-circulation/WorkingCirculation/EnvironmeentVariablesManagement/bin/Debug/net8.0/EnvironmeentVariablesManagement ^
+--templates-directory "environment-variables-example-files" ^
+--destination-directory  "environment-variables-files" ^
+--environment-variables-source-directory  "environment-variables-source"  ^
+--feature-name "wonderful feeature" ^
+--executive-file-directory "/EnvironmeentVariablesManagement/bin/Debug/net8.0/EnvironmeentVariablesManagement" ^
+--scripts-directory "" ^
+--repository-directory "" ^
+--hosting-directory "" 
+
+pause
