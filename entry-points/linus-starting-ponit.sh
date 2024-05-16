b936813680eed175050fa2c990a51edc8c3a3221
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-16 14:30:05 +0200

    creates linux entry point to call environment variables management execute file with arguments (#106)

---
+++ entry-points/linus-starting-ponit.sh
@@ -0,0 +1,9 @@
+/home/karim/Documents/GitHub/working-circulation/WorkingCirculation/EnvironmeentVariablesManagement/bin/Debug/net8.0/EnvironmeentVariablesManagement \
+--templates-directory "environment-variables-example-files" \
+--destination-directory  "environment-variables-files" \
+--environment-variables-source-directory  "environment-variables-source"  \
+--feature-name "wonderful feeature" \
+--executive-file-directory "/EnvironmeentVariablesManagement/bin/Debug/net8.0/EnvironmeentVariablesManagement"  \
+--scripts-directory "/home/karim/Documents/GitHub/working-circulation/scripts" \
+--repository-directory "/home/karim/Documents/GitHub/working-circulation" \
+--hosting-directory "/home/karim/Documents"
