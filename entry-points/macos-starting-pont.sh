3268d8f3bcfb1ac54014d85ee1082879860ae82b
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-16 14:31:57 +0200

    creates macos entry point to call environment variables management execute file with arguments (#107)

--- entry-points/macos-starting-pont.sh
+++ entry-points/macos-starting-pont.sh
@@ -1,9 +1,9 @@
+/Users/karimbeiranvand/Documents/GitHub/working-circulation/WorkingCirculation/EnvironmeentVariablesManagement/bin/Debug/net8.0/EnvironmeentVariablesManagement \
 --templates-directory "environment-variables-example-files" \
 --destination-directory  "environment-variables-files" \
---environment-variables-source-directory  "environment-variables-source"
---feature-name "wonderful feeature"
+--environment-variables-source-directory  "environment-variables-source"  \
+--feature-name "wonderful feeature" \
 --executive-file-directory "/EnvironmeentVariablesManagement/bin/Debug/net8.0/EnvironmeentVariablesManagement"  \
-### origins: everytthing  else is build on  top of these two pillars
 --scripts-directory "/Users/karimbeiranvand/Documents/GitHub/working-circulation/scripts" \
---repository-directory "/Users/karimbeiranvand/Documents/GitHub/working-circulation"
---hosting-directory "/Users/karimbeiranvand"
+--repository-directory "/Users/karimbeiranvand/Documents/GitHub/working-circulation" \
+--hosting-directory "/Users/karimbeiranvand" 
