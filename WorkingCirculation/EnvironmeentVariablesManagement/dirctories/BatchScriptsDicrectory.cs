65997ced0712c9d9d04a90c129559dfc0b19cb42
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-17 18:25:51 +0200

    19

--- WorkingCirculation/EnvironmeentVariablesManagement/dirctories/BatchScriptsDicrectory.cs
+++ WorkingCirculation/EnvironmeentVariablesManagement/dirctories/BatchScriptsDicrectory.cs
@@ -5,7 +5,27 @@ namespace EnvironmentVariablesManagement
 {
     internal class BatchScriptsDicrectory 
     {
-        public static void CopyToTargetDicrectory(IConfiguration configuration){
+        internal static void replaceFileNamesWithPaths()
+        {            
+            string pathToTarget = TargetDirectory.CreatePathToSelf();
+            string giversPath = PowerShellScriptsDirectory.ConstructPathToSelfInTargetDirectory();
+            foreach (string filePath in Directory.EnumerateFiles(pathToTarget)) 
+            {
+                string fileName = Path.GetFileNameWithoutExtension(filePath);
+                string giverFileName = $"""{fileName}.ps1""";
+                string giverPath = Path.Combine(giversPath, giverFileName);
+                Directories.replaceFileNameWithPath(filePath, giverPath);
+            }
+        }
+
+        public static string ConstructPathToSelfInTargetDirectory(string direcName)
+        {
+            string destinationDirectory = TargetDirectory.CreatePathToSelf();
+            string environmentVariablesFilesDirectory = Path.Combine(destinationDirectory, direcName);
+            return environmentVariablesFilesDirectory;
+        }
+
+        public static void CopyContentToTargetDicrectory(IConfiguration configuration){
             string sourceDirectory = CreatePathToSelf(configuration);
             string destinationDirectory = TargetDirectory.CreatePathToSelf();
 
