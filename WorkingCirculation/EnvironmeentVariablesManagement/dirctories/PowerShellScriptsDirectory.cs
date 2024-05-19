485070166394513d811b040dcae482ad3482cc76
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-19 17:33:42 +0200

    creates separate class for power shell scripts directory (#130)

---
+++ WorkingCirculation/EnvironmeentVariablesManagement/dirctories/PowerShellScriptsDirectory.cs
@@ -0,0 +1,43 @@
+using Microsoft.Extensions.Configuration;
+
+namespace EnvironmentVariablesManagement
+{
+    internal class PowerShellScriptsDirectory
+    {
+        public static void CopyContentToTargetDicrectory(IConfiguration configuration){
+            string direcName = "powershell-scripts";
+            Directory.CreateDirectory(ConstructPathToSelfInTargetDirectory(direcName));
+            string sourceDirectory = ConstructPathToSelfInScriptsDirectory(configuration, direcName);
+            string destinationDirectory = ConstructPathToSelfInTargetDirectory(direcName);
+
+            Directories.CopyContentOfSourceDirectoryToDestinationDirectory(sourceDirectory, destinationDirectory);
+        }
+        public static string ConstructPathToSelfInScriptsDirectory(IConfiguration configuration, string direcName)
+        {
+            string scriptsDirectoryName = SriptsDirectory.GetName(configuration);
+            string environmentVariablesFilesDirectory = Path.Combine(scriptsDirectoryName, direcName);
+            return environmentVariablesFilesDirectory;
+        }
+
+        public static string ConstructPathToSelfInTargetDirectory(string direcName)
+        {
+            string destinationDirectory = TargetDirectory.CreatePathToSelf();
+            string environmentVariablesFilesDirectory = Path.Combine(destinationDirectory, direcName);
+            return environmentVariablesFilesDirectory;
+        }
+
+        public static string ConstructPathToSelfInScriptsDirectory(IConfiguration configuration)
+        {
+            string scriptsDirectoryName = SriptsDirectory.GetName(configuration);
+            string environmentVariablesFilesDirectory = Path.Combine(scriptsDirectoryName, "powershell-scripts");
+            return environmentVariablesFilesDirectory;
+        }
+
+        public static string ConstructPathToSelfInTargetDirectory()
+        {
+            string destinationDirectory = TargetDirectory.CreatePathToSelf();
+            string environmentVariablesFilesDirectory = Path.Combine(destinationDirectory, "powershell-scripts");
+            return environmentVariablesFilesDirectory;
+        }
+    }
+}
