bdfc25c8a4c70c3296a190609349b09f057975ca
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-26 19:02:46 +0200

    put  "front end host address" "front end guest address" "web links address" "tools address" environment variables into directories power shell script (#151)

--- scripts/powershell-scripts/directories.ps1
+++ scripts/powershell-scripts/directories.ps1
@@ -6,5 +6,9 @@ get-content "directories.env" | ForEach-Object {
 }
 
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:FEND_ADDRESS
+start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:FEND_HOST_ADDRESS
+start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:FEND_GUEST_ADDRESS
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:BEND_ADDRESS
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:CALLS_ADDRESS
+start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:WEB_LINKS_ADDRESS
+start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:TOOLS_ADDRESS
