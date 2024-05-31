87d7d8addf7e532730db2302caf8043e0ba830dc
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-31 15:18:19 +0200

    add global.json from extract javascript object from edge console logs to .gitignore (#154)

--- WorkingCirculation/ExtractJavascriptObjectFromEdgeConsoleLogs/Program.cs
+++ WorkingCirculation/ExtractJavascriptObjectFromEdgeConsoleLogs/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        try
+        {
+            string workingDirectory = Directory.GetCurrentDirectory();
+            string phraseToFind = Environment.GetCommandLineArgs()[1];
+
+            Console.WriteLine(workingDirectory);
+            Console.WriteLine(phraseToFind);
+
+            foreach (string file in Directory.EnumerateFiles(workingDirectory))
+            {
+                Console.WriteLine(file);
+
+                using var readText = new StreamReader(file);
+                var currentLine = readText.ReadLine();
+                if (currentLine != null)
+                {
+                    if (currentLine.Contains(phraseToFind))
+                    {
+                        Console.WriteLine(currentLine);
+                    }
+                }
+            }  
+        }
+        catch (System.Exception exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+      
     }
 }
