512e49f9679432634c637976cb07d87e1c52e9ac
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-05-31 15:22:58 +0200

    some small improvements to scripts and entry points (#155)

--- scripts/powershell-scripts/receive-latest-changes-of-all-projects.ps1
+++ scripts/powershell-scripts/receive-latest-changes-of-all-projects.ps1
@@ -8,6 +8,3 @@ get-content "receive-latest-changes-of-all-projects.env" | ForEach-Object {
 start-process -FilePath $env:RECEIVER_LOCATION -ArgumentList "checkout develop" -Wait
 start-process -FilePath $env:RECEIVER_LOCATION -ArgumentList "pull" -Wait
 
-Write-Host -NoNewline  'press any key to coontinue ...'
-$null = $Host.UI.RawUI.ReadKey('NoEcho, IncludedKeyDown')
-
