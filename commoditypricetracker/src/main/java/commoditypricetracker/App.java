f18932a095e2bfddaa9fce4eb48851feeff7620c
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-06-07 20:31:45 +0200

    JavaFX GUI creating a basic window (#170)

--- commoditypricetracker/src/main/java/commoditypricetracker/App.java
+++ commoditypricetracker/src/main/java/commoditypricetracker/App.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,21 +16,27 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static Button button;
+    private static StackPane layout;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+
+        button = new Button();
+        button.setText("click me");
+        
+        layout = new StackPane();
+        layout.getChildren().add(button);
+        
+        scene = new Scene(layout, 640, 480);
+        
         stage.setScene(scene);
+        stage.setTitle("title of window");
         stage.show();
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
