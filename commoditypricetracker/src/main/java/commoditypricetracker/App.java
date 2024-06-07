1d16a66c9450e9014433c16ec80c6e3e84b27e8d
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-06-07 20:34:53 +0200

    JavaFX GUI switching scenes (#174)

--- commoditypricetracker/src/main/java/commoditypricetracker/App.java
+++ commoditypricetracker/src/main/java/commoditypricetracker/App.java
@@ -2,8 +2,10 @@ package commoditypricetracker;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -12,32 +14,40 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-    private static Button button;
-    private static StackPane layout;
+    private static Stage window;
+    private static Scene scene1, scene2;
 
     @Override
     public void start(Stage stage) throws IOException {
+        window = stage;
+        Label label1 = new Label("welcome to the first scene");
+        Button button1 = new Button("go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        button = new Button();
-        button.setText("click me");
-        button.setOnAction(e -> System.out.println("hello from other side"));
-        
-        layout = new StackPane();
-        layout.getChildren().add(button);
-        
-        scene = new Scene(layout, 640, 480);
-        
-        stage.setScene(scene);
-        stage.setTitle("title of window");
-        stage.show();
-    }
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1,button1);
 
-    static void setRoot(String fxml) throws IOException {
+        scene1 = new Scene(layout1, 400,400);
+
+
+        Button button2 = new Button("go back to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().addAll(button2);
+
+        scene2 = new Scene(layout2, 500,500);
+
+        window.setScene(scene1);
+        window.setTitle("window title");
+        window.show();
+
     }
 
     public static void main(String[] args) {
         launch();
     }
+
+    static void setRoot(String fxml) throws IOException {}
 
 }
