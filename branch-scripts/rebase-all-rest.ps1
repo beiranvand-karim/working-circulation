6cb3b9ebda843bc0c6c537e7e9e806c8850b8aff
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-06-11 16:01:43 +0200

    65

---
+++ branch-scripts/rebase-all-rest.ps1
@@ -0,0 +1,14 @@
+
+
+62 | ForEach-Object {$_} {
+    $current_branch = ($_ - 0)
+    git rebase master $current_branch
+    git push -f --set-upstream origin $current_branch
+}
+
+65 | ForEach-Object {$_} {
+    $current_branch = ($_ - 0)
+    $previous_branch = ($_ - 1)
+    git rebase $previous_branch $current_branch
+    git push -f --set-upstream origin $current_branch
+}
--- branch-scripts/remove-all-branches-local.ps1
+++ branch-scripts/remove-all-branches-local.ps1
@@ -1,9 +1,5 @@
 git checkout master
 
 1..10 | ForEach-Object {$_} {
-    # remote
-    git push -d origin $_
-
-    # local
     git branch -D $_
 }
