b4b475c7d2763f6a77ac86fa75e17bc02df4e0d6
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-06-21 20:49:04 +0200

    JavaFX GUI embedding layouts (#185)

--- commoditypricetracker/src/main/java/commoditypricetracker/App.java
+++ commoditypricetracker/src/main/java/commoditypricetracker/App.java
@@ -3,7 +3,10 @@ package commoditypricetracker;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -24,11 +27,27 @@ public class App extends Application {
             closeProgram();
         });
 
+        HBox topMenu =new HBox();
+        Button butttonA = new Button("File");
+        Button butttonB = new Button("Edit");
+        Button butttonC = new Button("View");
+        topMenu.getChildren().addAll(butttonA,butttonB, butttonC);
+
+
+        VBox leftMenu =new VBox();
+        Button butttonD = new Button("D");
+        Button butttonE = new Button("E");
+        Button butttonF = new Button("F");
+        leftMenu.getChildren().addAll(butttonD,butttonE, butttonF);
+
         buttton = new Button("Close program");
         buttton.setOnAction(e-> closeProgram());
-        StackPane stackPane =  new StackPane();
-        stackPane.getChildren().add(buttton);
-        Scene scene = new Scene(stackPane, 300,300);
+
+        BorderPane borderPane =  new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
+
+        Scene scene = new Scene(borderPane, 300,300);
         scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         window.setScene(scene);
         window.show();
