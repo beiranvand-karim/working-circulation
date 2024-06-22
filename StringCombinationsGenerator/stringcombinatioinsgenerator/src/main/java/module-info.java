952d91f95fe792380f0ed445a4c24cd3ecfbb5e7
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-06-22 20:31:29 +0200

    1

--- StringCombinationsGenerator/stringcombinatioinsgenerator/src/main/java/module-info.java
+++ StringCombinationsGenerator/stringcombinatioinsgenerator/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module stringcombinatioinsgenerator {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires transitive javafx.graphics;
+    
     opens stringcombinatioinsgenerator to javafx.fxml;
     exports stringcombinatioinsgenerator;
 }
