2886fa7d4b9dc01f36773986483fb76c28de5b9e
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-06-28 15:54:44 +0200

    Update App.java

--- commoditypricetracker/src/main/java/commoditypricetracker/App.java
+++ commoditypricetracker/src/main/java/commoditypricetracker/App.java
@@ -1,12 +1,8 @@
 package commoditypricetracker;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.io.IOException;
