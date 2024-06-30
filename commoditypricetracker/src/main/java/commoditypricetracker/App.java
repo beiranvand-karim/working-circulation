59e6079f2194233e68ac551146e70ee169a6a889
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-06-30 10:00:59 +0200

    JavaFX GUI binding properties example (#198)

--- commoditypricetracker/src/main/java/commoditypricetracker/App.java
+++ commoditypricetracker/src/main/java/commoditypricetracker/App.java
@@ -3,9 +3,14 @@ package commoditypricetracker;
 import javafx.application.Application;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -21,6 +26,20 @@ public class App extends Application {
         window = stage;
         window.setTitle("the new window");
 
+
+        TextField  userInpuTextField = new TextField();
+        userInpuTextField.setMaxWidth(200);
+
+        Label firstLabel  = new Label("Welcome to the site, ");
+        Label secoondLabel  = new Label();
+
+        HBox bottomTextHBox  = new HBox(firstLabel,  secoondLabel);
+        bottomTextHBox.setAlignment(Pos.CENTER);
+
+        VBox vBox = new VBox(10, userInpuTextField, bottomTextHBox);
+        vBox.setAlignment(Pos.CENTER);
+
+        secoondLabel.textProperty().bind(userInpuTextField.textProperty());
 
         IntegerProperty x= new SimpleIntegerProperty(3);
         IntegerProperty y= new SimpleIntegerProperty();
@@ -57,7 +76,7 @@ public class App extends Application {
         StackPane layout = new StackPane();
         layout.getChildren().add(submiButton);
 
-        Scene scene = new Scene(layout, 600,300);
+        Scene scene = new Scene(vBox, 600,300);
         scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         window.setScene(scene);
         window.show();
