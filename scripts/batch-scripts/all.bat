4872a6eaeacda9f7b820aee0b049eab6c23c3ac9
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-01 16:55:57 +0200

    umbrella script to run all (#194)

---
+++ scripts/batch-scripts/all.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REM all
+REM all.bat
+
+Powershell.exe -ExecutionPolicy RemoteSigned -File ".\powershell-scripts\all.ps1"
---
+++ scripts/powershell-scripts/all.ps1
@@ -0,0 +1,21 @@
+<#> fina = file name = all.ps1 #>
+
+& "$(Split-Path $MyInvocation.MyCommand.Path)/receive-latest-changes-of-all-projects.ps1"
+Start-Sleep -Seconds 10
+& "$(Split-Path $MyInvocation.MyCommand.Path)/directories.ps1"
+Start-Sleep -Seconds 10
+& "$(Split-Path $MyInvocation.MyCommand.Path)/infrastructure.ps1"
+Start-Sleep -Seconds 10
+& "$(Split-Path $MyInvocation.MyCommand.Path)/application.ps1"
+Start-Sleep -Seconds 10
+& "$(Split-Path $MyInvocation.MyCommand.Path)/application-stops.ps1"
+Start-Sleep -Seconds 10
+& "$(Split-Path $MyInvocation.MyCommand.Path)/rider-host-application.ps1"
+Start-Sleep -Seconds 10
+& "$(Split-Path $MyInvocation.MyCommand.Path)/rider-guest-application.ps1"
+Start-Sleep -Seconds 10
+& "$(Split-Path $MyInvocation.MyCommand.Path)/webstorm-host-clientapp.ps1"
+Start-Sleep -Seconds 10
+& "$(Split-Path $MyInvocation.MyCommand.Path)/webstorm-guest-clientapp.ps1"
+Start-Sleep -Seconds 10
+& "$(Split-Path $MyInvocation.MyCommand.Path)/application-user-interface.ps1"
