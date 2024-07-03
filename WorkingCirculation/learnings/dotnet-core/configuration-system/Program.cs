9d821f9eee36e5f61fe00c87fc63a9e427e5fc54
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-03 14:41:49 +0200

    9

--- WorkingCirculation/learnings/dotnet-core/configuration-system/Program.cs
+++ WorkingCirculation/learnings/dotnet-core/configuration-system/Program.cs
@@ -1,2 +1,18 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static void BuildConfig(IConfigurationBuilder builder)
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Prodcution"}.json", optional: true)
+            .AddEnvironmentVariables();
+    }
+}
