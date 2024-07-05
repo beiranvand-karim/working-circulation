9c345f2351fff9c816eafdc24fafef9f5d9e6f2d
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-05 16:48:35 +0200

    setting up dependency injection logging and setting (#216)

--- learnings/dotnet-core/configuration-system/Program.cs
+++ learnings/dotnet-core/configuration-system/Program.cs
@@ -1,11 +1,34 @@
 ﻿
+using configuration_system;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        Log.Logger.Information("Application starting ...");
+
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) => {
+                services.AddTransient<IGreetingService,GreetingService>();
+            })
+            .UseSerilog()
+            .Build();
+
+        var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+        svc.Run();
     }
 
     static void BuildConfig(IConfigurationBuilder builder)
