ef81eae85785cf2c0e5a616e962b7fd7efb4d403
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-05 16:53:33 +0200

    20

--- branch-scripts/rebase-only-one-to-master.ps1
+++ branch-scripts/rebase-only-one-to-master.ps1
@@ -2,6 +2,7 @@
 
 64 | ForEach-Object {$_} {
     $current_branch = ($_ - 0);
+    git fetch -f origin master:master;
     git rebase master $current_branch;
     git push -f --set-upstream origin $current_branch;
 }
