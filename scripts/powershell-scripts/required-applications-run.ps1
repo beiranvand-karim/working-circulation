61b1edffe1633b230d209491761c312c535437c3
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-09 10:51:37 +0200

    add notepad plus plus to required applications run script (#220)

--- scripts/powershell-scripts/required-applications-run.ps1
+++ scripts/powershell-scripts/required-applications-run.ps1
@@ -10,3 +10,5 @@ Start-Sleep -Seconds 3
 start-process -FilePath $env:OUTLOOK_EMAIL_CLIENT_LOCATION
 Start-Sleep -Seconds 3
 start-process -FilePath $env:GITHUB_DESKTOP_LOCATION
+Start-Sleep -Seconds 3
+start-process -FilePath $env:NOTEPADDPP_EXECUTE_FILE_LOCATION
