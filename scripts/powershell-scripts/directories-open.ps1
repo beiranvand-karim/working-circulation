257b3d984a59287b4e929cb221ea9d51d9eaeca9
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-13 19:20:23 +0200

    create time delay so that directories open in specified order (#231)

--- scripts/powershell-scripts/directories-open.ps1
+++ scripts/powershell-scripts/directories-open.ps1
@@ -6,9 +6,16 @@ get-content "directories-open.env" | ForEach-Object {
 }
 
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:FEND_ADDRESS
+Start-Sleep -Seconds 2
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:FEND_HOST_ADDRESS
+Start-Sleep -Seconds 2
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:FEND_GUEST_ADDRESS
+Start-Sleep -Seconds 2
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:BEND_ADDRESS
+Start-Sleep -Seconds 2
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:CALLS_ADDRESS
-start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:WEB_LINKS_ADDRESS
+Start-Sleep -Seconds 2
+start-process -FilePath$env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:WEB_LINKS_ADDRESS
+Start-Sleep -Seconds 2
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:TOOLS_ADDRESS
+Start-Sleep -Seconds 2
