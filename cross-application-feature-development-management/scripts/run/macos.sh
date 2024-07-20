dd033d41f35bed89af2e7a75cea712663fe14f88
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-20 23:24:28 +0200

    run scripts and worker files for "cross application feature development management" (#245)

--- cross-application-feature-development-management/scripts/run/macos.sh
+++ cross-application-feature-development-management/scripts/run/macos.sh
@@ -1,1 +1,11 @@
-dotnet run
+dotnet run \
+--templates-directory "environment-variables-template-files" \
+--destination-directory  "environment-variables-files" \
+--environment-variables-source-directory  "environment-variables-source"  \
+--feature-name "wonderful feature-v3" \
+--executive-file-directory "/cross-application-feature-development-management/bin/Debug/net8.0/cross-application-feature-development-management"  \
+--scripts-directory "[ ... fill in here ...]/working-circulation/scripts" \
+--repository-directory "[ ... fill in here ...]/working-circulation" \
+--hosting-directory "[ ... fill in here ...]" \
+--host-application-name "[ ... fill in here ...]" \
+--guest-application-name "[ ... fill in here ...]"
