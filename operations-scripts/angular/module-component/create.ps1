4c257b151646ad0a71810f4a74bf75999c74a8c9
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-21 17:02:05 +0200

    57

--- operations-scripts/angular/module-component/create.ps1
+++ operations-scripts/angular/module-component/create.ps1
@@ -1,7 +1,11 @@
 # file name: create.ps1
 # https://angular.dev/cli/generate/component#Options
 
-$name = "      ";
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$JsonFileContent = Join-Path -Path $ThisScript -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
+
+$name = $json.name;
 
 $name | ForEach-Object { $_ } {
     $RefinedName = $_.TrimStart().TrimEnd().Replace(" ", "-")
