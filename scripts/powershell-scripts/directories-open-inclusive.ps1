e64aac06e6ed9ddddf4f8f6fab24bb29f9fb7180
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-23 13:30:22 +0200

    58

--- scripts/powershell-scripts/directories-open-inclusive.ps1
+++ scripts/powershell-scripts/directories-open-inclusive.ps1
@@ -17,7 +17,7 @@ start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -Argume
 Start-Sleep -Seconds 2
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:CALLS_ADDRESS
 Start-Sleep -Seconds 2
-start-process -FilePath$env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:WEB_LINKS_ADDRESS
+start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:WEB_LINKS_ADDRESS
 Start-Sleep -Seconds 2
 start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList $env:TOOLS_ADDRESS
 Start-Sleep -Seconds 2
