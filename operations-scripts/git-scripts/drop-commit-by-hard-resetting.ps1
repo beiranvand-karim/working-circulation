0953d7ffde1843a94f2d566d7113f798a0c63d7c
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-24 17:49:23 +0200

    tell git scripts to read input parameters from input worker json file (#247)

--- operations-scripts/git-scripts/drop-commit-by-hard-resetting.ps1
+++ operations-scripts/git-scripts/drop-commit-by-hard-resetting.ps1
@@ -1,6 +1,10 @@
 # file name: drop-commit-by-hard-resetting.ps1
 
-$CommitIndexNumber = "        ";
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$JsonFileContent = Join-Path -Path $ThisScript -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
+
+$CommitIndexNumber = $json.CommitIndexNumber;
 $RefinedCommitIndexNumber = [int]$CommitIndexNumber.TrimStart().TrimEnd();
 
 $RefinedCommitIndexNumber | ForEach-Object { $_ } {
--- operations-scripts/git-scripts/remove-all-branches-local.ps1
+++ operations-scripts/git-scripts/remove-all-branches-local.ps1
@@ -1,6 +1,10 @@
 # file name: remove-all-branches-local.ps1
 
-$BranchIndexNumber = "        ";
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$JsonFileContent = Join-Path -Path $ThisScript -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
+
+$BranchIndexNumber = $json.BranchIndexNumber;
 $RefinedBranchIndexNumber = [int]$BranchIndexNumber.TrimStart().TrimEnd();
 
 git checkout master;
