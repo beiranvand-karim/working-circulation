2679a8331e314eaccca10d467b6beef2b2904309
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-26 18:00:39 +0200

    64

--- operations-scripts/git/branch/rebase/current-to-master-rest-to-previous.ps1
+++ operations-scripts/git/branch/rebase/current-to-master-rest-to-previous.ps1
@@ -2,7 +2,8 @@
 
 $ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
 $ThisScriptParent = Split-Path -Path $ThisScript -Parent;
-$JsonFileContent = Join-Path -Path $ThisScriptParent -ChildPath "input.worker.json";
+$ThisScriptGrandParent = Split-Path -Path $ThisScriptParent -Parent;
+$JsonFileContent = Join-Path -Path $ThisScriptGrandParent -ChildPath "input.worker.json";
 $json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
 
 $BranchIndexNumber = $json.BranchIndexNumber;
