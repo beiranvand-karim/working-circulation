81790b2a6f19510edf00bc0169ee695eb1159b72
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-26 18:00:39 +0200

    64

--- operations-scripts/git/commit/amend.ps1
+++ operations-scripts/git/commit/amend.ps1
@@ -1,15 +1,15 @@
 # file name: amend.ps1
 
 $thisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
-$jsonData1 = Join-Path -Path $thisScript -ChildPath "input.worker.json";
-$json = Get-Content -Path $jsonData1 -Raw | ConvertFrom-Json;
+$ThisScriptParent = Split-Path -Path $ThisScript -Parent;
+$JsonFileContent = Join-Path -Path $ThisScriptParent -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
 
 $CommitIndexNumber = $json.CommitIndexNumber;
 $BranchIndexNumber = $json.BranchIndexNumber;
 
 $RefinedCommitIndexNumber = [int]$CommitIndexNumber.TrimStart().TrimEnd();
 $RefinedBranchIndexNumber = [int]$BranchIndexNumber.TrimStart().TrimEnd();
-
 
 $RefinedCommitIndexNumber | ForEach-Object { $_ } {
     git reset --soft HEAD~$_;
