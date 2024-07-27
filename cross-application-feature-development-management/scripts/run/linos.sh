6b90dcc9c3f6f535bc95add6ab1e3462fffdc178
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-27 21:31:43 +0200

    69

--- cross-application-feature-development-management/scripts/run/linos.sh
+++ cross-application-feature-development-management/scripts/run/linos.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+hosting_directory=${PWD}
+
 dotnet run \
 --templates-directory "environment-variables-template-files" \
 --destination-directory  "environment-variables-files" \
@@ -6,6 +10,6 @@ dotnet run \
 --executive-file-directory "/cross-application-feature-development-management/bin/Debug/net8.0/cross-application-feature-development-management"  \
 --scripts-directory "[ ... fill in here ...]/working-circulation/scripts" \
 --repository-directory "[ ... fill in here ...]/working-circulation" \
---hosting-directory "[ ... fill in here ...]" \
+--hosting-directory "${hosting_directory}" \
 --host-application-name "[ ... fill in here ...]" \
 --guest-application-name "[ ... fill in here ...]"
