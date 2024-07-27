612c21179a0fa616d4cf45133e5850e04c57079e
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-07-27 20:10:25 +0200

    66

--- cross-application-feature-development-management/Dirctories/FeatureNameDirectory.cs
+++ cross-application-feature-development-management/Dirctories/FeatureNameDirectory.cs
@@ -1,14 +1,17 @@
 using cross_application_feature_development_management.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace cross_application_feature_development_management.Dirctories
 {
     public class FeatureNameDirectory(
             ICommandLineArgs commandLineArgs,
-            IHostingDirectory hostingDirectory
+            IHostingDirectory hostingDirectory,
+            IConfiguration configuration
         ): IFeatureNameDirectory
     {
         private readonly ICommandLineArgs commandLineArgs = commandLineArgs;
         private readonly IHostingDirectory hostingDirectory = hostingDirectory;
+        private readonly IConfiguration configuration = configuration;
 
         public void CreateSelf()
         {
@@ -22,6 +25,8 @@ namespace cross_application_feature_development_management.Dirctories
         {
             string featureNameDirectoryNameKey = commandLineArgs.GetKey("FeatureNameKey");
             string featureNameDirectoryName = commandLineArgs.GetByKey(featureNameDirectoryNameKey);
+            string value = configuration["--FeatureName"] ?? "default";
+            Console.WriteLine(value);
             string hostingDirectoryName = hostingDirectory.GetName();
             string featureNameDirectoryPath = Path.Combine(hostingDirectoryName, featureNameDirectoryName);
             return featureNameDirectoryPath;
