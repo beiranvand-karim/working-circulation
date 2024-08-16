de1f864127600baa3014075c5152b60ce6c5a411
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-08-16 14:50:53 +0200

    change "edit-last-commit.ps1" to "commit/last/edit.ps1" (#260)

--- operations-scripts/git/commit/last/edit.ps1
+++ operations-scripts/git/commit/last/edit.ps1
@@ -1,8 +1,9 @@
-# file name: edit-last-commit.ps1
+# file name: edit.ps1
 
-$thisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
 $ThisScriptParent = Split-Path -Path $ThisScript -Parent;
-$JsonFileContent = Join-Path -Path $ThisScriptParent -ChildPath "input.worker.json";
+$ThisScriptGrandParent = Split-Path -Path $ThisScriptParent -Parent;
+$JsonFileContent = Join-Path -Path $ThisScriptGrandParent -ChildPath "input.worker.json";
 $json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
 
 $CommitIndexNumber = $json.CommitIndexNumber;
