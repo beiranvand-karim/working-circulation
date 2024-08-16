925865f4bd47c7ddb4c2e8da7db5d8f182756047
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-08-16 15:30:52 +0200

    79

---
+++ operations-scripts/git/branch/current/commit/create-temp10min.ps1
@@ -0,0 +1,8 @@
+# file name: create-temp10min.ps1
+
+$cmdOutput = git branch --show-current | Out-String
+$my_string = [string]::join("", ($cmdOutput.Split("`n")))
+$temp10min = "$my_string-temp10min";
+git checkout -b $temp10min;
+git add .;
+git commit -m "$temp10min";
