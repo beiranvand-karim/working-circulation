a9aa449eb4e012c18e80bb6e84483fbc99e85805
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-08-23 21:25:00 +0200

    108

---
+++ operations-scripts/git/create/next/branch/from/current/branch/and/push .ps1
@@ -0,0 +1,11 @@
+# file name: /create/from/current/branch.ps1
+
+$cmdOutput = git branch --show-current | Out-String
+$CurrentBranchName = [string]::join("", ($cmdOutput.Split("`n")))
+$CurrentBranchIndexNumber = [int] $CurrentBranchName;
+$NextBranchCommitIndexNumber = ($CurrentBranchIndexNumber + 1);
+
+git checkout -b $NextBranchCommitIndexNumber;
+git add .;
+git commit -m "$NextBranchCommitIndexNumber";
+git push --set-upstream origin $NextBranchCommitIndexNumber;
