2ee8843a797ef69be2eb2d89561f1361c5b5af7c
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-08-25 20:37:01 +0200

    112

--- cross-application-feature-development-management/operations/run/linos.sh
+++ cross-application-feature-development-management/operations/run/linos.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 hosting_directory=${PWD}
+github_desktop_directory=
+host_application_name=
+guest_application_name=
 
 dotnet run \
 --templates-directory "environment-variables-template-files" \
@@ -8,8 +11,8 @@ dotnet run \
 --environment-variables-source-directory  "environment-variables-source"  \
 --feature-name "wonderful feature-v3" \
 --executive-file-directory "/cross-application-feature-development-management/bin/Debug/net8.0/cross-application-feature-development-management"  \
---scripts-directory "[ ... fill in here ...]/working-circulation/cross-application-feature-development-management/scripts" \
---repository-directory "[ ... fill in here ...]/working-circulation" \
---hosting-directory "${hosting_directory}" \
---host-application-name "[ ... fill in here ...]" \
---guest-application-name "[ ... fill in here ...]"
+--scripts-directory "${github_desktop_directory}/working-circulation/cross-application-feature-development-management/scripts" \
+--repository-directory "${github_desktop_directory}/working-circulation" \
+--hosting-directory "${hosting_directory}/workers" \
+--host-application-name "${host_application_name}" \
+--guest-application-name "${guest_application_name}"
