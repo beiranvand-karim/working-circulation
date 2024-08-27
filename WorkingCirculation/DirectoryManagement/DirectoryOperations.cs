f42991216b113bbedae41b607e8c194c3290d87a
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-08-27 16:20:31 +0200

    115

--- WorkingCirculation/DirectoryManagement/DirectoryOperations.cs
+++ WorkingCirculation/DirectoryManagement/DirectoryOperations.cs
@@ -1,12 +1,23 @@
 using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 namespace DirectoryManagement
 {
-    internal class DirectoryOperations {
-        public static void OpenDirectoryThroughExplorer(string directoryPath){
-            if(Directory.Exists(directoryPath))
+    public interface IDirectoryOperations
+    {
+        public void OpenDirectoryThroughExplorer(string directoryPath);
+
+        public void OpenDirectoryThroughCommandLine(string commandToExecute, string workingDirectory);
+    }
+    public class DirectoryOperations(ILogger<DirectoryOperations> logger) : IDirectoryOperations
+    {
+        private readonly ILogger<DirectoryOperations> logger = logger;
+
+        public void OpenDirectoryThroughExplorer(string directoryPath)
+        {
+            if (Directory.Exists(directoryPath))
             {
-                ProcessStartInfo startInfo = new ProcessStartInfo
+                ProcessStartInfo startInfo = new()
                 {
                     Arguments = directoryPath,
                     FileName = "explorer.exe"
@@ -16,12 +27,13 @@ namespace DirectoryManagement
 
         }
 
-        public static void OpenDirectoryThroughCommandLine(string commandToExecute, string workingDirectory) {
-            Process process = new Process();
+        public void OpenDirectoryThroughCommandLine(string commandToExecute, string workingDirectory)
+        {
+            Process process = new();
             process.StartInfo.WorkingDirectory = workingDirectory;
             process.StartInfo.FileName = "cmd.exe";
             process.StartInfo.Arguments = $"/c {commandToExecute}";
-            process.StartInfo.RedirectStandardOutput= true;
+            process.StartInfo.RedirectStandardOutput = true;
             process.Start();
             process.WaitForExit();
             string output = process.StandardOutput.ReadToEnd();
