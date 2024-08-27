87f14f6a8b3d062cf8a4e2317af59b0ffe242c03
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-08-27 16:20:31 +0200

    115

--- WorkingCirculation/DirectoryManagement/Program.cs
+++ WorkingCirculation/DirectoryManagement/Program.cs
@@ -1,15 +1,43 @@
 ﻿using DirectoryManagement;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
-string choice = "2";
-string workingDirectory = Environment.GetCommandLineArgs()[1];
-
-if(choice == "1") 
+internal class Program
 {
-    string commandToExecute = Environment.GetCommandLineArgs()[2];
-    DirectoryOperations.OpenDirectoryThroughCommandLine(commandToExecute, workingDirectory);
-}
+    private static void Main(string[] args)
+    {
 
-if(choice == "2") 
-{
-    DirectoryOperations.OpenDirectoryThroughExplorer(workingDirectory);
+        var builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                services.AddTransient<IDirectoryManager, DirectoryManager>();
+                services.AddTransient<IDirectoryOperations, DirectoryOperations>();
+            })
+            .UseSerilog()
+            .Build();
+
+        var svc = ActivatorUtilities.CreateInstance<DirectoryManager>(host.Services);
+        svc.Run();
+
+    }
+
+
+    static void BuildConfig(IConfigurationBuilder builder)
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Prodcution"}.json", optional: true)
+            .AddEnvironmentVariables();
+    }
 }
