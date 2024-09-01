d3a0fd73e52255d871af6e167f926dd248fca96e
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-09-01 17:20:14 +0200

    119

--- cross-application-feature-development-management/CrossApplicationFeatureDevelopmentManagement.cs
+++ cross-application-feature-development-management/CrossApplicationFeatureDevelopmentManagement.cs
@@ -57,43 +57,7 @@ namespace cross_application_feature_development_management
                             environmentVariablesSourceDirectory.GetName()
                         );
 
-                foreach (string templateFile in Directory.EnumerateFiles(templateSourceDirectory))
-                {
-                    string destFileName = Path.GetFileNameWithoutExtension(templateFile);
-                    string destFile = Path.Combine(destinationDirectory, destFileName);
-                    using var fs = File.Create(destFile);
 
-                    Dictionary<string, string> contentToWrite = [];
-
-                    if (templateFile.Contains("directories"))
-                    {
-                        contentToWrite =
-                        somethingFeatureNameDirectory.PairUpVariablesWithTheirValue(templateFile, environmentVariablesSourceDictionary);
-                    }
-                    else if (templateFile.Contains("startup"))
-                    {
-                        contentToWrite =
-                        addToStartupScript.PairUpVariablesWithTheirValue(templateFile, environmentVariablesSourceDictionary);
-                    }
-                    else if (templateFile.Contains("notepadpp-open-all"))
-                    {
-                        contentToWrite =
-                        notePadPlusPlusOpenAll.PairUpVariablesWithTheirValue(templateFile, environmentVariablesSourceDictionary);
-                    }
-                    else
-                    {
-                        contentToWrite =
-                        something.PairUpVariablesWithTheirValue(templateFile, environmentVariablesSourceDictionary);
-                    }
-
-                    using StreamWriter writer = new(fs);
-                    foreach (KeyValuePair<string, string> entry in contentToWrite)
-                    {
-                        string valueToWrite = $"""{entry.Key}={entry.Value}""";
-                        writer.WriteLine(valueToWrite);
-                    }
-
-                }
                 powerShellScriptsDirectory.CopyContentToFeatureNameDicrectory();
                 powerShellScriptsDirectory.ReplaceFileNamesWithPaths();
 
