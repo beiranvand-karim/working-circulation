5e45fc868bab24dad94914c1dbe741d43064fdd0
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-09-20 16:39:32 +0200

    124

--- dotnet/notepad-plus-plus-file-management/terminal/notepad-plus-plus-file-management/Program.cs
+++ dotnet/notepad-plus-plus-file-management/terminal/notepad-plus-plus-file-management/Program.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using notepad_plus_plus_file_management;
 using notepad_plus_plus_file_management.Interfaces;
 using notepad_plus_plus_file_management.Dirctories.Interfaces;
 using notepad_plus_plus_file_management.Dirctories.Classes;
@@ -12,44 +11,52 @@ using notepad_plus_plus_file_management.Files.Executabes.Interfaces;
 using notepad_plus_plus_file_management.Files.Executabes.Classes;
 using notepad_plus_plus_file_management.Helpers.Interfaces;
 using notepad_plus_plus_file_management.Helpers.Classes;
+using notepad_plus_plus_file_management.Dirctories.Feature.AutomationsDirectory;
+using notepad_plus_plus_file_management.Dirctories.Feature.AutomationsDirectory.ProcessesMetaDataDirectory;
 
-internal class Program
+namespace notepad_plus_plus_file_management
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        var builder = new ConfigurationBuilder();
-        BuildConfig(builder);
+        private static void Main(string[] args)
+        {
 
-        Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(builder.Build())
-            .Enrich.FromLogContext()
-            .WriteTo.Console()
-            .CreateLogger();
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
 
-        var host = Host.CreateDefaultBuilder()
-            .ConfigureServices((context, services) =>
-            {
-                services.AddTransient<IProcessManager, ProcessManager>();
-                services.AddTransient<INotePadPlusPlus, NotePadPlusPlus>();
-                services.AddTransient<IDirectoryOperations, DirectoryOperations>();
-                services.AddTransient<ICommandLineArgs, CommandLineArgs>();
-                services.AddTransient<IFeatureNameDirectory, FeatureNameDirectory>();
-                services.AddTransient<IHostingDirectory, HostingDirectory>();
-                services.AddTransient<IDirectoriesNameToKeyMap, DirectoriesNameToKeyMap>();
-                services.AddTransient<IFrontEndHostDirectory, FrontEndHostDirectory>();
-                services.AddTransient<IFrontEndGuestDirectory, FrontEndGuestDirectory>();
-                services.AddTransient<INotesAndMessagesDirectory, NotesAndMessagesDirectory>();
-                services.AddTransient<IStringHelpers, StringHelpers>();
-            })
-            .UseSerilog()
-            .Build();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
 
-        var svc = ActivatorUtilities.CreateInstance<ProcessManager>(host.Services);
-        svc.Run();
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<IProcessManager, ProcessManager>();
+                    services.AddTransient<INotePadPlusPlus, NotePadPlusPlus>();
+                    services.AddTransient<IDirectoryOperations, DirectoryOperations>();
+                    services.AddTransient<ICommandLineArgs, CommandLineArgs>();
+                    services.AddTransient<IFeatureNameDirectory, FeatureNameDirectory>();
+                    services.AddTransient<IHostingDirectory, HostingDirectory>();
+                    services.AddTransient<IDirectoriesNameToKeyMap, DirectoriesNameToKeyMap>();
+                    services.AddTransient<IFrontEndHostDirectory, FrontEndHostDirectory>();
+                    services.AddTransient<IFrontEndGuestDirectory, FrontEndGuestDirectory>();
+                    services.AddTransient<INotesAndMessagesDirectory, NotesAndMessagesDirectory>();
+                    services.AddTransient<IStringHelpers, StringHelpers>();
+                    services.AddTransient<IAutomationsDirectory, AutomationsDirectory>();
+                    services.AddTransient<IProcessesMetaDataDirectory, ProcessesMetaDataDirectory>();
+                })
+                .UseSerilog()
+                .Build();
+
+            var svc = ActivatorUtilities.CreateInstance<ProcessManager>(host.Services);
+            svc.Run();
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder) => builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Prodcution"}.json", optional: true)
+                .AddEnvironmentVariables();
     }
-
-    static void BuildConfig(IConfigurationBuilder builder) => builder.SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Prodcution"}.json", optional: true)
-            .AddEnvironmentVariables();
 }
