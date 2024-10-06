f9a20347af5fcf355852ed0f3ae02d94997ecd5b
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-10-06 15:50:54 +0200

    126

--- cross-application-feature-development-management/scripts/powershell-scripts/notepadpp-open-all.ps1
+++ cross-application-feature-development-management/scripts/powershell-scripts/notepadpp-open-all.ps1
@@ -5,6 +5,6 @@ get-content "notepadpp-open-all.env" | ForEach-Object {
     set-content env:\$name $value
 }
 Push-Location $env:NOTEPAD_PLUS_PLUS_FILE_MANAGEMENT_EXECUTIVE_FILE_CONTAINING_DIRECTORY
-$ArgumentList = "--notepaddpp-execute-file-location $env:NOTEPADDPP_EXECUTE_FILE_LOCATION --feature-name $env:FEATURE_NAME  --hosting-directory $env:HOSTING_DIRECTORY --host-application-name $env:HOST_APPLICATION_NAME --guest-application-name $env:GUEST_APPLICATION_NAME"
+$ArgumentList = "--command $env:COMMAND --notepaddpp-execute-file-location $env:NOTEPADDPP_EXECUTE_FILE_LOCATION --feature-name $env:FEATURE_NAME  --hosting-directory $env:HOSTING_DIRECTORY --host-application-name $env:HOST_APPLICATION_NAME --guest-application-name $env:GUEST_APPLICATION_NAME"
 start-process -FilePath $env:NOTEPAD_PLUS_PLUS_FILE_MANAGEMENT_EXECUTIVE_FILE_LOCATION -ArgumentList $ArgumentList
 Pop-Location
