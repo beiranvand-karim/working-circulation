9fd6f30b4332938f866020c0654d99d544a170e1
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-10-12 22:44:25 +0200

    131

---
+++ cross-application-feature-development-management/scripts/batch-scripts/all-inclusive-order-reverse.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM all-inclusive-order-reverse
+REM all-inclusive-order-reverse.bat
+
+Powershell.exe -ExecutionPolicy RemoteSigned -File "all-inclusive-order-reverse.ps1"
+all-inclusive-order-reverse
