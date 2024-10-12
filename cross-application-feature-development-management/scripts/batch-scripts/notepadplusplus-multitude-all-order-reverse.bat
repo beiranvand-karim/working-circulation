7ef57700644e60cc6eef83facb28c395effb3fc9
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-10-12 22:44:25 +0200

    131

---
+++ cross-application-feature-development-management/scripts/batch-scripts/notepadplusplus-multitude-all-order-reverse.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REM notepadplusplus-multitude-all-order-reverse
+REM notepadplusplus-multitude-all-order-reverse.bat
+
+Powershell.exe -ExecutionPolicy RemoteSigned -File "notepadplusplus-multitude-all-order-reverse.ps1"
