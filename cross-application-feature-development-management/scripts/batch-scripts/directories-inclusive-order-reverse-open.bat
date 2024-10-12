21c13c8c0de6618744c647ec96782c71a8648c27
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-10-12 22:44:25 +0200

    131

---
+++ cross-application-feature-development-management/scripts/batch-scripts/directories-inclusive-order-reverse-open.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REM directories-inclusive-order-reverse-open
+REM directories-inclusive-order-reverse-open.bat
+
+Powershell.exe -ExecutionPolicy RemoteSigned -File "directories-inclusive-order-reverse-open.ps1"
