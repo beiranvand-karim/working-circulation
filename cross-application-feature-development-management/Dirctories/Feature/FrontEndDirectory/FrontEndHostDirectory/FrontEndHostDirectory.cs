2047af114ba0e20436b767f908575157b1841cad
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-11-06 17:04:12 +0100

    133

---
+++ cross-application-feature-development-management/Dirctories/Feature/FrontEndDirectory/FrontEndHostDirectory/FrontEndHostDirectory.cs
@@ -0,0 +1,37 @@
+using cross_application_feature_development_management.Dirctories.Interfaces;
+using cross_application_feature_development_management.Helpers.Interfaces;
+using cross_application_feature_development_management.Interfaces;
+using Microsoft.Extensions.Logging;
+
+namespace cross_application_feature_development_management.Dirctories.Feature.FrontEndDirectory.FrontEndHostDirectory
+{
+    public class FrontEndHostDirectory(
+            IDirectoriesNameToKeyMap directoriesNameToKeyMap,
+            IFeatureNameDirectory featureNameDirectory,
+            ICommandLineArgs commandLineArgs,
+            ILogger<FrontEndHostDirectory> logger,
+            IStringHelpers stringHelpers
+        ) : IFrontEndHostDirectory
+    {
+        private readonly IDirectoriesNameToKeyMap directoriesNameToKeyMap = directoriesNameToKeyMap;
+        private readonly IFeatureNameDirectory featureNameDirectory = featureNameDirectory;
+        private readonly ICommandLineArgs commandLineArgs = commandLineArgs;
+        private readonly ILogger<FrontEndHostDirectory> logger = logger;
+        private readonly IStringHelpers stringHelpers = stringHelpers;
+
+        public string GetPath(string key)
+        {
+            var directoryName = directoriesNameToKeyMap.GetValue(key);
+            var featureNameDirectoryPath = featureNameDirectory.GetPath();
+            var directoryThatIsGoingToBeOpen = Path.Combine(featureNameDirectoryPath, directoryName);
+
+            var hostApplicationName = commandLineArgs.GetByKey("--host-application-name");
+
+            var x = string.Format("{0}.{1}", directoryName, hostApplicationName);
+
+            var directoryThatIsGoingToBeOpen2 = Path.Combine(directoryThatIsGoingToBeOpen, x);
+            logger.LogInformation("front end host directory: {FrontEndHostDirectory}", directoryThatIsGoingToBeOpen2);
+            return directoryThatIsGoingToBeOpen2;
+        }
+    }
+}
