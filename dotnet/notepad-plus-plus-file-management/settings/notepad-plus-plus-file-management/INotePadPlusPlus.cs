519600dfde7cd4965a8bc35d7c939016c6a6c955
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-12-22 22:23:01 +0100

    "/dotnet/.../settings/notepad-plus-plus-file-management" project creation (#304)

---
+++ dotnet/notepad-plus-plus-file-management/settings/notepad-plus-plus-file-management/INotePadPlusPlus.cs
@@ -0,0 +1,8 @@
+namespace notepad_plus_plus_file_management
+{
+    public interface INotePadPlusPlus
+    {
+        public string GetExceutiveFileLocation();
+        public string GetOpeneeFilesContainingDirectoryLocation();
+    }
+}
