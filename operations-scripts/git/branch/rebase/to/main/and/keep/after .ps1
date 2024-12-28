c7ea05d85f344196c1f3f0ee6cf11819f4b64a03
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-12-28 18:01:25 +0100

    "git/branch/rebase/to/main/and/keep/after" and "git/branch/rebase/to/main/and/push/after" operations scripts (#354)

---
+++ operations-scripts/git/branch/rebase/to/main/and/keep/after .ps1
@@ -0,0 +1,16 @@
+# file name: rebase-only-one-to-master.ps1
+
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$ThisScriptParent = Split-Path -Path $ThisScript -Parent;
+$ThisScriptGrandParent = Split-Path -Path $ThisScriptParent -Parent;
+$JsonFileContent = Join-Path -Path $ThisScriptGrandParent -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
+
+$BranchIndexNumber = $json.BranchIndexNumber;
+$RefinedBranchIndexNumber = [int]$BranchIndexNumber.TrimStart().TrimEnd();
+
+$RefinedBranchIndexNumber | ForEach-Object { $_ } {
+    $current_branch = ($_ - 0);
+    git fetch -f origin main:main;
+    git rebase main $current_branch;
+}
