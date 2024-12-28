c72e481f83f23b480e4f81ca1ebd756aa24a974b
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-12-28 17:29:21 +0100

    "cafdem ... all-inclusive-order-reverse.bat" and "cafdem ... all-include-nothing-order-reverse.bat" and all sub-scripts (#349)

---
+++ cross-application-feature-development-management/scripts/batch-scripts/directories-include-nothing-order-reverse-action-open.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REM directories-include-nothing-order-reverse-action-open
+REM directories-include-nothing-order-reverse-action-open.bat
+
+Powershell.exe -ExecutionPolicy RemoteSigned -File "directories-include-nothing-order-reverse-action-open.ps1"
