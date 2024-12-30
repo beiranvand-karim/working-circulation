e13778876e8e799dbfcd7be4fc9f9bc058349a65
Karim Beiranvand <beiranvand.karim@gmail.com>
2024-12-30 19:04:42 +0100

    143

---
+++ cross-application-feature-development-management/ProccessInformation.cs
@@ -0,0 +1,8 @@
+namespace cross_application_feature_development_management
+{
+    public class ProccessInformation
+    {
+        public string? GroupName { get; set; }
+        public int? Id { get; set; }
+    }
+}
