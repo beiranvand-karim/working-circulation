b624d48038498f2d8bb24427b947c86fa92192a9
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-01-01 17:52:46 +0100

    84

--- cross-application-feature-development-management/scripts/powershell-scripts/required-applications-run.ps1
+++ cross-application-feature-development-management/scripts/powershell-scripts/required-applications-run.ps1
@@ -8,4 +8,4 @@ get-content "required-applications-run.env" | ForEach-Object {
 start-process -FilePath $env:DOCKER_DESKTOP_LOCATION
 start-process -FilePath $env:OUTLOOK_EMAIL_CLIENT_LOCATION -ArgumentList "/recycle"
 start-process -FilePath $env:GITHUB_DESKTOP_LOCATION
-start-process -FilePath $env:ZSCALER_CLIENT_CONNECTOR
+start-process -FilePath $env:ZSCALER_CLIENT_CONNECTOR -ArgumentList "-shortcut"
