03bf36a57c6af02f9a067c111a16d44199be1da4
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-01-11 18:16:59 +0100

    149

--- cross-application-feature-development-management/Dirctories/Interfaces/IEnvironmentVariablesFilesDirectory.cs
+++ cross-application-feature-development-management/Dirctories/Interfaces/IEnvironmentVariablesFilesDirectory.cs
@@ -2,8 +2,8 @@ namespace cross_application_feature_development_management.Dirctories.Interfaces
 {
     public interface IEnvironmentVariablesFilesDirectory
     {
-        public void CopyContentToFeatureNameDicrectory();
-        public void CopyContentToTargetDicrectory();
+        public void CopyContentToFeatureNameDirectory();
+        public void CopyContentToTargetDirectory();
         public string CreatePathToSelfInScriptsDirectory();
         public string CreatePathToSelfInFeatureNameDirectory();
         public string CreatePathToSelfInTargetDirectory();
--- cross-application-feature-development-management/ProccessInformation.cs
+++
@@ -1,8 +0,0 @@
-namespace cross_application_feature_development_management
-{
-    public class ProccessInformation
-    {
-        public string? GroupName { get; set; }
-        public int? Id { get; set; }
-    }
-}
