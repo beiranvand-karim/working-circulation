a3ce10ba087d559e62d180b512c36c90f9f3fc3a
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-02-20 06:40:57 +0100

    177

---
+++ cross-application-feature-development-management/scripts/batch-scripts/ide-jetbrains-rider-multitude-host-action-open.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REM ide-jetbrains-rider-multitude-host-action-open
+REM ide-jetbrains-rider-multitude-host-action-open.bat
+
+Powershell.exe -ExecutionPolicy RemoteSigned -File "ide-jetbrains-rider-multitude-host-action-open.ps1"
