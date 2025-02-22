9f6cadcb4f3fc07abb61a4ef052b9de240794814
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-02-22 10:36:50 +0100

    183

--- cross-application-feature-development-management/scripts/powershell-scripts/ide-jetbrains-rider-multitude-primary-action-open.ps1
+++ cross-application-feature-development-management/scripts/powershell-scripts/ide-jetbrains-rider-multitude-primary-action-open.ps1
@@ -5,4 +5,20 @@ get-content "ide-jetbrains-rider-multitude-primary-action-open.env" | ForEach-Ob
     set-content env:\$name $value
 }
 
-start-process -FilePath $env:RIDER_LOCATION -ArgumentList $env:PRIMARY_APPLICATION_LOCATION
+$ArgumentList=@"
+--application $env:APPLICATION `
+--command $env:COMMAND `
+--ide-execute-file-location $env:RIDER_LOCATION `
+--application-location $env:PRIMARY_APPLICATION_LOCATION `
+--ide-name $env:IDE_NAME `
+--application-name $env:HOST_APPLICATION_NAME `
+--feature-name $env:FEATURE_NAME `
+--hosting-directory $env:HOSTING_DIRECTORY
+"@
+
+Push-Location $env:CAFDEM_EXECUTIVE_FILE_ADDRESS_CONTAINING_DIRECTORY
+
+start-process -FilePath $env:CAFDEM_EXECUTIVE_FILE_ADDRESS `
+    -ArgumentList $ArgumentList
+
+Pop-Location
