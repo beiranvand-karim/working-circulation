5737d4eb704e5309e56600b1ba8f34f9f2afc120
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-02-23 17:06:55 +0100

    188

--- cross-application-feature-development-management/scripts/powershell-scripts/ide-jetbrains-webstrom-multitude-secondary-action-open.ps1
+++
@@ -1,8 +0,0 @@
-<#> fina = file name = ide-jetbrains-webstrom-multitude-secondary-action-open.ps1 #>
-
-get-content "ide-jetbrains-webstrom-multitude-secondary-action-open.env" | ForEach-Object {
-    $name, $value = $_.split("=")
-    set-content env:\$name $value
-}
-
-start-process -FilePath $env:WEBSTORM_LOCATION -ArgumentList $env:SECONDARY_CLIENTAPP_LOCATION
