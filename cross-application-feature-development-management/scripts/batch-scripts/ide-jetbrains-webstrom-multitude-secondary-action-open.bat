cf90405c876082c69ce46941a0f93cb2df34e0b6
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-02-23 17:06:55 +0100

    route ide-jetbrains-webstorm-multitude-secondary-action-open through cafdem

--- cross-application-feature-development-management/scripts/batch-scripts/ide-jetbrains-webstrom-multitude-secondary-action-open.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REM ide-jetbrains-webstrom-multitude-secondary-action-open
-REM ide-jetbrains-webstrom-multitude-secondary-action-open.bat
-
-Powershell.exe -ExecutionPolicy RemoteSigned -File "ide-jetbrains-webstrom-multitude-secondary-action-open.ps1"
