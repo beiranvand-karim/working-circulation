a6608d92fb70c2985accd8aad262d4d5c5f7a479
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-03-07 16:39:32 +0100

    210

--- cross-application-feature-development-management/scripts/powershell-scripts/docker-network-application-multitude-all-action-start.ps1
+++ cross-application-feature-development-management/scripts/powershell-scripts/docker-network-application-multitude-all-action-start.ps1
@@ -7,6 +7,11 @@ get-content "docker-network-application-multitude-all-action-start.env" | ForEac
 
 start-process -FilePath $env:DOCKER_CLI_LOCATION -ArgumentList "-SwitchWindowsEngine"
 push-location $env:APPLICATION_COMPOSE_FILE_LOCATION
+az acr login --name $env:DOCKER_LOGIN_NAME
+$AZURE_CLIENT_SECRET = AZURE_CLIENT_SECRET_FROM_ENVIRONMENT_VARIABLES
+$env:AZURE_CLIENT_SECRET = $AZURE_CLIENT_SECRET
+docker compose down
 docker compose pull
 docker compose up -d
+
 pop-location
