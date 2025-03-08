6d13b46c78dc74c33e1ef73e352454257d7ba8fb
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-03-08 18:56:27 +0100

    219

--- cross-application-feature-development-management/scripts/batch-scripts/docker-network-application-multitude-primary-action-stop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REM docker-network-application-multitude-primary-action-stop
-REM docker-network-application-multitude-primary-action-stop.bat
-
-Powershell.exe -ExecutionPolicy RemoteSigned -File "docker-network-application-multitude-primary-action-stop.ps1"
--- cross-application-feature-development-management/scripts/batch-scripts/docker-network-application-multitude-secondary-action-stop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REM ddocker-network-application-multitude-secondary-action-stop
-REM docker-network-application-multitude-secondary-action-stop.bat
-
-Powershell.exe -ExecutionPolicy RemoteSigned -File "docker-network-application-multitude-secondary-action-stop.ps1"
