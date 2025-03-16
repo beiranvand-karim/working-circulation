c80a685e3b02ec31fa5c9e8e4866a6ae5852026d
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-03-16 19:16:36 +0100

    249

--- cross-application-feature-development-management/scripts/powershell-scripts/aggregate-all-multitude-serving-order-recto-action-open.ps1
+++ cross-application-feature-development-management/scripts/powershell-scripts/aggregate-all-multitude-serving-order-recto-action-open.ps1
@@ -5,7 +5,7 @@ get-content "aggregate-all-multitude-serving-order-recto-action-open.env" | ForE
     set-content env:\$name $value
 }
 
-& "$(Split-Path $MyInvocation.MyCommand.Path)/required-applications-run.ps1"
+& "$(Split-Path $MyInvocation.MyCommand.Path)/aggregate-required-action-open.ps1"
 Start-Sleep -Seconds 10
 & "$(Split-Path $MyInvocation.MyCommand.Path)/directories-open.ps1"
 Start-Sleep -Seconds 10
