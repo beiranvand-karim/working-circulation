b596d4347b55e6cf41fde0d24bde77e018974d19
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-03-16 18:06:09 +0100

    239

--- cross-application-feature-development-management/scripts/powershell-scripts/dotnet-multitude-secondary-order-recto-action-run.ps1
+++ cross-application-feature-development-management/scripts/powershell-scripts/dotnet-multitude-secondary-order-recto-action-run.ps1
@@ -1,3 +1,3 @@
-<#> fina = file name = run-secondary-application.ps1 #>
+<#> fina = file name = dotnet-multitude-secondary-order-recto-action-run.ps1 #>
 
 Invoke-Expression 'cmd /c start powershell -Command {pwsh "run-guest-application.ps1";}'
--- cross-application-feature-development-management/scripts/powershell-scripts/run-multitude-all-order-reverse.ps1
+++ cross-application-feature-development-management/scripts/powershell-scripts/run-multitude-all-order-reverse.ps1
@@ -1,5 +1,5 @@
 <#> fina = file name = run-multitude-all-order-reverse.ps1 #>
 
-& "$(Split-Path $MyInvocation.MyCommand.Path)/run-secondary-application.ps1"
+& "$(Split-Path $MyInvocation.MyCommand.Path)/dotnet-multitude-secondary-order-recto-action-run.ps1"
 Start-Sleep -Seconds 5
 & "$(Split-Path $MyInvocation.MyCommand.Path)/dotnet-multitude-primary-order-recto-action-run.ps1"
