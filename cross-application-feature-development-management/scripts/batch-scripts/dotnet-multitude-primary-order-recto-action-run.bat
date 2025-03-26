a5de5af232c73f2afe0eb374272f55b4d610a45b
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-03-26 17:33:35 +0100

    255

--- cross-application-feature-development-management/scripts/batch-scripts/dotnet-multitude-primary-order-recto-action-run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REM dotnet-multitude-primary-order-recto-action-run
-REM dotnet-multitude-primary-order-recto-action-run.bat
-
-Powershell.exe -ExecutionPolicy RemoteSigned -File "dotnet-multitude-primary-order-recto-action-run.ps1"
