977694e5d211a589f2946cedab8170bcda8b63f7
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-03-26 17:38:16 +0100

    256

--- cross-application-feature-development-management/scripts/batch-scripts/dotnet-multitude-secondary-order-recto-action-run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REM dotnet-multitude-secondary-order-recto-action-run
-REM dotnet-multitude-secondary-order-recto-action-run.bat
-
-Powershell.exe -ExecutionPolicy RemoteSigned -File "dotnet-multitude-secondary-order-recto-action-run.ps1"
