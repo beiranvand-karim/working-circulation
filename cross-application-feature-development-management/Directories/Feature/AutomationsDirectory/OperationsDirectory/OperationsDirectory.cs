9e1626795a28dfe9c9290469206f806ca60cb8f6
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-03-28 17:51:39 +0100

    259

--- cross-application-feature-development-management/Directories/Feature/AutomationsDirectory/OperationsDirectory/OperationsDirectory.cs
+++ cross-application-feature-development-management/Directories/Feature/AutomationsDirectory/OperationsDirectory/OperationsDirectory.cs
@@ -1,7 +1,8 @@
 namespace cross_application_feature_development_management.Directories.Feature.AutomationsDirectory.OperationsDirectory
 {
     public class OperationsDirectory(
-        AutomationsDirectory automationsDirectory
+        AutomationsDirectory automationsDirectory,
+        Directories directories
         )
     {
         public void Create()
@@ -14,6 +15,18 @@ namespace cross_application_feature_development_management.Directories.Feature.A
             var directory = automationsDirectory.GetPath();
             var operationsDirectory = Path.Combine(directory, "operations");
             return operationsDirectory;
+        }
+
+        public void ReplaceFileNamesWithPaths(string giversPath)
+        {
+            var pathToTarget = GetPath();
+            foreach (var filePath in Directory.EnumerateFiles(pathToTarget))
+            {
+                var fileName = Path.GetFileNameWithoutExtension(filePath);
+                var giverFileName = $"{fileName}.ps1";
+                var giverPath = Path.Combine(giversPath, giverFileName);
+                directories.ReplaceFileNameWithPath(filePath, giverPath);
+            }
         }
     }
 }
