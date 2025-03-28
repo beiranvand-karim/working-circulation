f4affdc2f25afc7c14122b95b5744c0e87b52622
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-03-28 17:51:39 +0100

    259

--- cross-application-feature-development-management/Directories/BatchScriptsDirectory.cs
+++ cross-application-feature-development-management/Directories/BatchScriptsDirectory.cs
@@ -13,50 +13,13 @@ namespace cross_application_feature_development_management.Directories
         OperationsDirectory operationsDirectory
         )
     {
-        public void ReplaceFileNamesWithPaths()
+        public void CopyContentToDirectory(string destinationDirectory)
         {
-            var pathToTarget = operationsDirectory.GetPath();
-            var giversPath = powerShellScriptsDirectory.GetPath();
-            foreach (var filePath in Directory.EnumerateFiles(pathToTarget))
-            {
-                var fileName = Path.GetFileNameWithoutExtension(filePath);
-                var giverFileName = $"{fileName}.ps1";
-                var giverPath = Path.Combine(giversPath, giverFileName);
-                directories.ReplaceFileNameWithPath(filePath, giverPath);
-            }
-        }
-
-        public string ConstructPathToSelfInTargetDirectory(string direcName)
-        {
-            var destinationDirectory = targetDirectory.CreatePathToSelf();
-            var environmentVariablesFilesDirectory = Path.Combine(destinationDirectory, direcName);
-            return environmentVariablesFilesDirectory;
-        }
-
-        public string CreatePathToSelfInFeatureNameDirector()
-        {
-            var scriptsDirectoryName = scriptsDirectory.GetName();
-            var batchScriptsDirectoryPath = Path.Combine(scriptsDirectoryName, "batch-scripts");
-            return batchScriptsDirectoryPath;
-        }
-
-        public void CopyContentToFeatureNameDirectory()
-        {
-            var sourceDirectory = CreatePathToSelfInScriptsDirectory();
-            var destinationDirectory = operationsDirectory.GetPath();
-
+            var sourceDirectory = GetPath();
             directories.CopyContentOfSourceDirectoryToDestinationDirectory(sourceDirectory, destinationDirectory);
         }
 
-        public void CopyContentToTargetDirectory()
-        {
-            var sourceDirectory = CreatePathToSelfInScriptsDirectory();
-            var destinationDirectory = targetDirectory.CreatePathToSelf();
-
-            directories.CopyContentOfSourceDirectoryToDestinationDirectory(sourceDirectory, destinationDirectory);
-        }
-
-        public string CreatePathToSelfInScriptsDirectory()
+        public string GetPath()
         {
             var scriptsDirectoryName = scriptsDirectory.GetName();
             var batchScriptsDirectoryPath = Path.Combine(scriptsDirectoryName, "batch-scripts");
