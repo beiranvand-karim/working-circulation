dbfe53fc3a1222314b38fe61e80d9e2e39002d91
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-04-05 15:36:59 +0200

    265

--- cross-application-feature-development-management/Directories/Scripts/EnvironmentVariablesSource/EnvironmentVariablesSourceDirectory.cs
+++ cross-application-feature-development-management/Directories/Scripts/EnvironmentVariablesSource/EnvironmentVariablesSourceDirectory.cs
@@ -1,19 +1,13 @@
 namespace cross_application_feature_development_management.Directories.Scripts.EnvironmentVariablesSource
 {
     public class EnvironmentVariablesSourceDirectory(
-        CommandLineArgs commandLineArgs,
         ScriptsDirectory scriptsDirectory
         )
     {
         public string GetPath()
         {
-            var environmentVariablesSourceDirectoryNameKey =
-               commandLineArgs.GetKey("EnvironmentVariablesSourceDirectoryNameKey");
-
-            var environmentVariablesSourceDirectoryName = CommandLineArgs.GetByKey(environmentVariablesSourceDirectoryNameKey);
-
             var environmentVariablesSourceDirectory =
-                Path.Combine(scriptsDirectory.GetPath(), environmentVariablesSourceDirectoryName);
+                Path.Combine(scriptsDirectory.GetPath(), "environment-variables-source");
 
             return environmentVariablesSourceDirectory;
         }
--- cross-application-feature-development-management/Directories/Scripts/ScriptsDirectory.cs
+++ cross-application-feature-development-management/Directories/Scripts/ScriptsDirectory.cs
@@ -1,14 +1,11 @@
 namespace cross_application_feature_development_management.Directories.Scripts
 {
-    public class ScriptsDirectory(
-            CommandLineArgs commandLineArgs
-        )
+    public class ScriptsDirectory
     {
         public string GetPath()
         {
-            var scriptsDirectoryNameKey = commandLineArgs.GetKey("ScriptsDirectoryNameKey");
-            var scriptsDirectoryName = CommandLineArgs.GetByKey(scriptsDirectoryNameKey);
-            return scriptsDirectoryName;
+            var scriptsDirectoryPath = CommandLineArgs.GetByKey("--scripts-directory");
+            return scriptsDirectoryPath;
         }
     }
 }
