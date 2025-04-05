1c1e6f06a5ec3d8f8e01676f161cfa03c328498c
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-04-05 15:36:59 +0200

    265

--- cross-application-feature-development-management/Directories/HostingDirectory/HostingDirectory.cs
+++ cross-application-feature-development-management/Directories/HostingDirectory/HostingDirectory.cs
@@ -1,14 +1,11 @@
 namespace cross_application_feature_development_management.Directories.HostingDirectory
 {
-    public class HostingDirectory(
-            CommandLineArgs commandLineArgs
-        )
+    public class HostingDirectory
     {
         public string GetPath()
         {
-            var hostingDirectoryNameKey = commandLineArgs.GetKey("HostingDirectoryNameKey");
-            var hostingDirectoryName = CommandLineArgs.GetByKey(hostingDirectoryNameKey);
-            return hostingDirectoryName;
+            var hostingDirectoryPath = CommandLineArgs.GetByKey("--hosting-directory");
+            return hostingDirectoryPath;
         }
     }
 }
