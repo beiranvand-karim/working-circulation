8698a7f119bd8953d6c93175ac318f7c7032dd6f
Karim Beiranvand <beiranvand.karim@gmail.com>
2025-04-19 19:34:04 +0200

    282

--- cross-application-feature-development-management/scripts/primary/powershell-scripts/directories-multitude-serving-order-reverse-action-open.ps1
+++ cross-application-feature-development-management/scripts/primary/powershell-scripts/directories-multitude-serving-order-reverse-action-open.ps1
@@ -9,37 +9,37 @@ Push-Location $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS_CONTAINING_DIRECT
 
 if ($env:IS_OPENING_FEND_GUEST_ADDRESS -eq $true ) {
     start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList "--directory-to-be-open $env:FEND_GUEST_ADDRESS"
-    Start-Sleep -Seconds 2
+    Start-Sleep -Millisecond 700
 }
 
 if ($env:IS_OPENING_FEND_HOST_ADDRESS -eq $true ) {
     start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList "--directory-to-be-open $env:FEND_HOST_ADDRESS"
-    Start-Sleep -Seconds 2
+    Start-Sleep -Millisecond 700
 }
 
 if ($env:IS_OPENING_BEND_ADDRESS -eq $true ) {
     start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList "--directory-to-be-open $env:BEND_ADDRESS"
-    Start-Sleep -Seconds 2
+    Start-Sleep -Millisecond 700
 }
 
 if ($env:IS_OPENING_CALLS_ADDRESS -eq $true ) {
     start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList "--directory-to-be-open $env:CALLS_ADDRESS"
-    Start-Sleep -Seconds 2
+    Start-Sleep -Millisecond 700
 }
 
 if ($env:IS_OPENING_TOOLS_ADDRESS -eq $true ) {
     start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList "--directory-to-be-open $env:TOOLS_ADDRESS"
-    Start-Sleep -Seconds 2
+    Start-Sleep -Millisecond 700
 }
 
 if ($env:IS_OPENING_NOTES_MESSAGES_ADDRESS -eq $true ) {
     start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList "--directory-to-be-open $env:NOTES_MESSAGES_ADDRESS"
-    Start-Sleep -Seconds 2
+    Start-Sleep -Millisecond 700
 }
 
 if ($env:IS_OPENING_WEB_LINKS_ADDRESS -eq $true ) {
     start-process -FilePath $env:DIRECTORY_MANAGEMENT_EXECUTIVE_FILE_ADDRESS -ArgumentList "--directory-to-be-open $env:WEB_LINKS_ADDRESS"
-    Start-Sleep -Seconds 2
+    Start-Sleep -Millisecond 700
 }
 
 Pop-Location
