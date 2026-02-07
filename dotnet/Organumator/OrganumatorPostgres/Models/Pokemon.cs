0e945d049e77dd1da4221faa9dc40e3640e6a90d
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-02-07 21:44:48 +0100

    527

--- dotnet/Organumator/OrganumatorPostgres/Models/Pokemon.cs
+++ dotnet/Organumator/OrganumatorPostgres/Models/Pokemon.cs
@@ -8,7 +8,7 @@ namespace organumator.Models
     public class Pokemon
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type  { get; set; }  
+        public string Name { get; set; } = string.Empty;
+        public string Type  { get; set; } = string.Empty;
     }
 }
