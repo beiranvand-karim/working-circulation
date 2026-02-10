24977f0dec66836f5a1eb1cb775298db3da3b0c0
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-02-10 14:49:34 +0100

    530

---
+++ dotnet/Organumator/OrganumatorMssql/Mappers/CommentMapper.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OrganumatorMssql.Dtos.Comment;
+using OrganumatorMssql.Models;
+
+namespace OrganumatorMssql.Mappers
+{
+    public static class CommentMapper
+    {
+
+        public static CommentDto ToCommentDto(this Comment comment)
+        {
+            return new CommentDto
+            {
+                Id = comment.Id,
+                Title = comment.Title,
+                Content = comment.Content,
+                CreatedOn = comment.CreatedOn,
+                StockId = comment.StockId
+            };
+        }
+
+        public static Comment ToCommentFromCreateDto(this CreateCommentDto commentDto, int stockId)
+        {
+            return new Comment
+            {
+                Title = commentDto.Title,
+                Content = commentDto.Content,
+                StockId = stockId
+            };
+        }
+    }
+}
