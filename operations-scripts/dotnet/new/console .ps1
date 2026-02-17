75ce0a69e9fd1d8fcd5e9dd18876571c0a1b1978
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-02-17 13:55:57 +0100

    534

--- operations-scripts/dotnet/new/console .ps1
+++ operations-scripts/dotnet/new/console .ps1
@@ -3,12 +3,12 @@ $ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
 $JsonFileContent = Join-Path -Path $ThisScript -ChildPath "input.worker.json";
 $json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
 
-$name = $json.Name_WebApi;
-$ParentDirectoryPath = $json.ParentDirectoryPath_WebApi;
+$name = $json.Name_Console;
+$ParentDirectoryPath = $json.ParentDirectoryPath_Console;
 $RefinedName = $name.TrimStart().TrimEnd().Replace(" ", "-");
 
 
 New-Item -Path "$ParentDirectoryPath" -ItemType "directory" -Name "$RefinedName";
 Push-Location "$ParentDirectoryPath/$RefinedName";
-dotnet new webapi --framework net8.0;
+dotnet new console --framework net8.0;
 Pop-Location;
