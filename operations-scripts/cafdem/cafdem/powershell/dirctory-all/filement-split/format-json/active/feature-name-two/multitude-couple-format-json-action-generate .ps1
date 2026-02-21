7bd77b957bc75c45bdc117091a4182d98cf08d8f
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-02-21 22:02:51 +0100

    536

--- operations-scripts/cafdem/cafdem/powershell/dirctory-all/filement-split/format-json/active/feature-name-two/multitude-couple-format-json-action-generate .ps1
+++ operations-scripts/cafdem/cafdem/powershell/dirctory-all/filement-split/format-json/active/feature-name-two/multitude-couple-format-json-action-generate .ps1
@@ -1,29 +1,28 @@
 
-$feature_name="wonderful feature"
-$primary_application_name="augustus"
-$secondary_application_name="decimus"
+$feature_name = "feature-name-two"
+$primary_application_name = "augustus"
+$secondary_application_name = "decimus"
 
-$repository_directory="/Users/karimbeiranvand/Documents/GitHub/working-circulation"
-$working_directory="$repository_directory/cross-application-feature-development-management"
-$hosting_directory="$working_directory/workers/features"
-$feature_directory="$working_directory/workers/features/$feature_name"
+$repository_directory = "/home/karim/Documents/GitHub/working-circulation"
+$working_directory = "$repository_directory/cross-application-feature-development-management"
+$hosting_directory = "/home/karim/Documents/feature-development"
+$feature_directory = "/home/karim/Documents/feature-development/$feature_name"
 
 Write-Host $feature_directory
-
-Remove-Item -Recurse -Force -Path $feature_directory
 
 Push-Location $working_directory
 
 dotnet run `
---application "cross-application-feature-development-management" `
---command "create-scripts" `
---format "json" `
---filement "split" `
---feature-name "$feature_name" `
---executive-file-directory "$working_directory/bin/Debug/net8.0/cross-application-feature-development-management" `
---repository-directory "$repository_directory" `
---hosting-directory "$hosting_directory" `
---primary-application-name "$primary_application_name" `
---secondary-application-name "$secondary_application_name"
+    --application "cross-application-feature-development-management" `
+    --command "create-scripts" `
+    --format "json" `
+    --filement "split" `
+    --code-base "codebase" `
+    --feature-name "$feature_name" `
+    --executive-file-directory "$working_directory/bin/Debug/net8.0/cross-application-feature-development-management" `
+    --repository-directory "$repository_directory" `
+    --hosting-directory "$hosting_directory" `
+    --primary-application-name "$primary_application_name" `
+    --secondary-application-name "$secondary_application_name"
 
 Pop-Location
