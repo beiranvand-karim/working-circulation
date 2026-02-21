59f577532cecd6291179b65d6db193e3f4f5dd09
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-02-21 22:02:51 +0100

    536

---
+++ operations-scripts/cafdem/directory-manangement/powershell/dirctory-all/filement-split/format-json/archive/feature-name-three/multitude-couple-action-open .ps1
@@ -0,0 +1,18 @@
+$feature_name = "feature-name-three"
+$primary_application_name = "augustus"
+$secondary_application_name = "decimus"
+$current_directory = $PWD.Path
+$hosting_directory = "/home/karim/Documents/feature-development"
+
+dotnet run `
+    --application "directory-management" `
+    --format "json" `
+    --command "open" `
+    --filement "split" `
+    --code-base "codebase" `
+    --feature-name "$feature_name" `
+    --executive-file-directory "$current_directory/bin/Debug/net8.0/cross-application-feature-development-management" `
+    --scripts-directory "$current_directory/scripts" `
+    --hosting-directory "$hosting_directory" `
+    --primary-application-name "$primary_application_name" `
+    --secondary-application-name "$secondary_application_name"
