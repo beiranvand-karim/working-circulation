6778cc9036f7485cad050484b87c2395e8e2ec5b
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-02-28 20:01:46 +0100

    541

---
+++ operations-scripts/angular/project/create-standalone .ps1
@@ -0,0 +1,17 @@
+# file name: create.ps1
+# https://angular.dev/cli/new#Options
+
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$JsonFileContent = Join-Path -Path $ThisScript -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
+
+$name = $json.name;
+$WorkingDirectory = $json.WorkingDirectoryForStandalone;
+Push-Location $WorkingDirectory;
+
+$name | ForEach-Object { $_ } {
+    $RefinedName = $_.TrimStart().TrimEnd().Replace(" ", "-")
+    ng new $RefinedName --skip-tests --style=scss --ssr=false --routing --inline-style=false --inline-template=false;
+}
+
+Pop-Location
