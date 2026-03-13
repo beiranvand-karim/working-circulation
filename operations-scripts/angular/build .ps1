641909846efc644c63636f737db7655091102ba9
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-03-13 19:10:44 +0100

    543

---
+++ operations-scripts/angular/build .ps1
@@ -0,0 +1,11 @@
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$JsonFileContent = Join-Path -Path $ThisScript -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
+
+$WorkingDirectory = $json.WorkingDirectory;
+
+Push-Location $WorkingDirectory
+
+ng build
+
+Pop-Location
---
+++ operations-scripts/dotnet/ef/database-update-connection-prod .ps1
@@ -0,0 +1,13 @@
+
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$JsonFileContent = Join-Path -Path $ThisScript -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
+
+$WorkingDirectory = $json.WorkingDirectory;
+$ConnectionStringProduction = $json.DefaultConnectionProduction;
+
+Push-Location $WorkingDirectory
+
+dotnet ef database update --connection $ConnectionStringProduction
+
+Pop-Location
