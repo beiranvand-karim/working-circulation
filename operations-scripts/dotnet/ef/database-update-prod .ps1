5d94b513f4f434fc6153660d612be517b8fe0314
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-03-13 19:10:44 +0100

    543

---
+++ operations-scripts/dotnet/ef/database-update-prod .ps1
@@ -0,0 +1,13 @@
+
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$JsonFileContent = Join-Path -Path $ThisScript -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
+
+$WorkingDirectory = $json.WorkingDirectory;
+
+Push-Location $WorkingDirectory
+
+$env:ASPNETCORE_ENVIRONMENT = "Production"
+dotnet ef database update
+
+Pop-Location
---
+++ operations-scripts/ubuntu/apt/remove .ps1
@@ -0,0 +1,7 @@
+$ThisScript = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent;
+$JsonFileContent = Join-Path -Path $ThisScript -ChildPath "input.worker.json";
+$json = Get-Content -Path $JsonFileContent -Raw | ConvertFrom-Json;
+
+$PackageName = $json.PackageName;
+
+apt remove $PackageName
