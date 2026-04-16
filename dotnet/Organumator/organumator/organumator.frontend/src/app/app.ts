384349526711e90aebfd2e2714b22a34420b9233
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-04-16 21:05:24 +0200

    544

--- dotnet/Organumator/organumator/organumator.frontend/src/app/app.ts
+++ dotnet/Organumator/organumator/organumator.frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AroundBrushing } from './around-brushing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AroundBrushing],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ dotnet/Organumator/organumator/organumator.frontend/src/app/around-brushing/models/around-brushing.model.ts
@@ -0,0 +1,6 @@
+
+
+export interface AroundBrushingItemModel {
+  id: number
+  performedOnDate: string
+}
