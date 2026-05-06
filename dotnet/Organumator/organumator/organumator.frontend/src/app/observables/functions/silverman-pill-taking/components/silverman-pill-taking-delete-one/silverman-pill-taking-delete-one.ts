4b0347dbbdee4e9cf320814c0aeaea25349d7b5f
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-05-06 17:55:21 +0200

    547

---
+++ dotnet/Organumator/organumator/organumator.frontend/src/app/observables/functions/silverman-pill-taking/components/silverman-pill-taking-delete-one/silverman-pill-taking-delete-one.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, inject, Input, OnDestroy, Output } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { SilvermanPillTakingService } from '../../services/silverman-pill-taking.service';
+
+@Component({
+  selector: 'silverman-pill-taking-delete-one',
+  imports: [],
+  templateUrl: './silverman-pill-taking-delete-one.html',
+  styleUrl: './silverman-pill-taking-delete-one.scss',
+  providers: [SilvermanPillTakingService],
+})
+export class SilvermanPillTakingDeleteOne implements OnDestroy {
+  @Input() itemId!: number
+  @Output() deleted = new EventEmitter<void>()
+
+  private readonly destroy$ = new Subject<void>()
+  private readonly silvermanPillTakingService = inject(SilvermanPillTakingService)
+  
+
+  deleteItem() {
+    this.silvermanPillTakingService.delete(this.itemId).pipe(
+      takeUntil(this.destroy$)
+    ).subscribe({
+      next: () => {
+        console.log(`Item with id ${this.itemId} deleted successfully`)
+        this.deleted.emit()
+      },
+      error: error =>
+        console.error(`Failed to delete item with id ${this.itemId}:`, error),
+    })
+
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
