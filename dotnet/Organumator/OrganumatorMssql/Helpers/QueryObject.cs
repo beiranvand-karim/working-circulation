4bc54ca9539e52935b847b2be8db5dc58ac1e416
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-05-13 14:50:05 +0200

    554

--- dotnet/Organumator/OrganumatorMssql/Helpers/QueryObject.cs
+++ dotnet/Organumator/OrganumatorMssql/Helpers/QueryObject.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace OrganumatorMssql.Helpers
 {
     public class QueryObject
     {
         public string Symbol { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
+
+        public string? SortBy { get; set; } = string.Empty;
+
+        public bool IsDescending { get; set; } = false;
     }
 }
--- dotnet/Organumator/OrganumatorMssql/Repositories/StockRepository.cs
+++ dotnet/Organumator/OrganumatorMssql/Repositories/StockRepository.cs
@@ -49,6 +49,18 @@ namespace OrganumatorMssql.Repositories
                 stocks = stocks.Where(s => s.Symbol.Contains(queryObject.Symbol));
             }
 
+            if (!string.IsNullOrWhiteSpace(queryObject.SortBy))
+            {
+                stocks = stocks.Where(s => s.Symbol.Contains(queryObject.Symbol));
+
+                if (queryObject.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = queryObject.IsDescending
+                        ? stocks.OrderByDescending(s => s.Symbol)
+                        : stocks.OrderBy(s => s.Symbol);
+                }
+            }
+
             return await stocks.ToListAsync();
 
         }
