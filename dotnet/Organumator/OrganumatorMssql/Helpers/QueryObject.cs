f3861c5d31aab093ef38d83125a12135f69ce26b
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-05-14 14:44:07 +0200

    555

--- dotnet/Organumator/OrganumatorMssql/Helpers/QueryObject.cs
+++ dotnet/Organumator/OrganumatorMssql/Helpers/QueryObject.cs
@@ -8,5 +8,9 @@ namespace OrganumatorMssql.Helpers
         public string? SortBy { get; set; } = string.Empty;
 
         public bool IsDescending { get; set; } = false;
+
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize { get; set; } = 20;
     }
 }
