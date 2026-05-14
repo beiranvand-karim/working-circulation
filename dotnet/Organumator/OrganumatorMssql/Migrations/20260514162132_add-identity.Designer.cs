d7c1b1b5fa6b9c84239954813ce0e5431448efa8
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-05-14 18:27:55 +0200

    556

--- dotnet/Organumator/OrganumatorMssql/Migrations/20260514162132_add-identity.Designer.cs
+++ dotnet/Organumator/OrganumatorMssql/Migrations/20260514162132_add-identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrganumatorMssql.Data;
 
@@ -11,9 +12,11 @@ using OrganumatorMssql.Data;
 namespace OrganumatorMssql.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260514162132_add-identity")]
+    partial class addidentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
