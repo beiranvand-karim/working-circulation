6dfa2fd06d35e41455715f692e530c070943a675
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-06-06 13:31:36 +0200

    595

--- dotnet/cafdemalihapa/scripts/powershell-scripts/notepadplusplus-multitude-all-order-reverse-action-open.ps1
+++ dotnet/cafdemalihapa/scripts/powershell-scripts/notepadplusplus-multitude-all-order-reverse-action-open.ps1
@@ -4,7 +4,20 @@ get-content "notepadplusplus-multitude-all-order-reverse-action-open.env" | ForE
     $name, $value = $_.split("=")
     set-content env:\$name $value
 }
+
+$ArgumentList =(
+    "--application $env:APPLICATION" +
+    " --command $env:COMMAND" +
+    " --order $env:ORDER" +
+    " --notepaddpp-execute-file-location $env:NOTEPADDPP_EXECUTE_FILE_LOCATION" +
+    " --feature-name $env:FEATURE_NAME" +
+    " --hosting-directory $env:HOSTING_DIRECTORY" +
+    " --primary-application-name $env:PRIMARY_APPLICATION_NAME" +
+    " --secondary-application-name $env:SECONDARY_APPLICATION_NAME"
+    )
+
 Push-Location $env:NOTEPAD_PLUS_PLUS_FILE_MANAGEMENT_EXECUTIVE_FILE_CONTAINING_DIRECTORY
-$ArgumentList = "--application $env:APPLICATION --command $env:COMMAND --order $env:ORDER --notepaddpp-execute-file-location $env:NOTEPADDPP_EXECUTE_FILE_LOCATION --feature-name $env:FEATURE_NAME  --hosting-directory $env:HOSTING_DIRECTORY --primary-application-name $env:PRIMARY_APPLICATION_NAME --secondary-application-name $env:SECONDARY_APPLICATION_NAME"
+
 start-process -FilePath $env:NOTEPAD_PLUS_PLUS_FILE_MANAGEMENT_EXECUTIVE_FILE_LOCATION -ArgumentList $ArgumentList
+
 Pop-Location
---
+++ operations-scripts/cafdemalihapa/notepad-plus-plus-file-management/active/feature-name-one/multitude-all-order-reverse-action-shut .ps1
@@ -0,0 +1,24 @@
+$feature_name = "feature-name-one"
+$primary_application_name = "augustus"
+$secondary_application_name = "decimus"
+$notepaddpp_execute_file_location = "C:\\Program Files\\Notepad++\\notepad++.exe"
+$repository_directory = "C:\\workplace\\GitHub\\working-circulation"
+$working_directory = "$repository_directory\\dotnet\\cafdemalihapa"
+$hosting_directory = "C:\\workplace\\feature-development"
+
+# $feature_directory = "$hosting_directory\\$feature_name"
+# Remove-Item -Recurse -Force -Path $feature_directory
+
+Push-Location $working_directory
+
+dotnet run `
+    --application "notepad-plus-plus-file-management" `
+    --command "close" `
+    --order "reverse" `
+    --feature-name "$feature_name" `
+    --notepaddpp-execute-file-location "$notepaddpp_execute_file_location" `
+    --hosting-directory "$hosting_directory" `
+    --primary-application-name "$primary_application_name" `
+    --secondary-application-name "$secondary_application_name"
+
+Pop-Location
