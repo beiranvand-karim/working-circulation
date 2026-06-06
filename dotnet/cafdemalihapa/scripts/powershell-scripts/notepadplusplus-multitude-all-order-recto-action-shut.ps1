b439cc2e2eda8d940253243ddebe163151bcd50e
Karim Beiranvand <beiranvand.karim@gmail.com>
2026-06-06 13:31:36 +0200

    595

--- dotnet/cafdemalihapa/scripts/powershell-scripts/notepadplusplus-multitude-all-order-recto-action-shut.ps1
+++ dotnet/cafdemalihapa/scripts/powershell-scripts/notepadplusplus-multitude-all-order-recto-action-shut.ps1
@@ -4,7 +4,19 @@ get-content "notepadplusplus-multitude-all-order-recto-action-shut.env" | ForEac
     $name, $value = $_.split("=")
     set-content env:\$name $value
 }
+
+$ArgumentList =(
+    "--application $env:APPLICATION" +
+    " --command $env:COMMAND" +
+    " --notepaddpp-execute-file-location $env:NOTEPADDPP_EXECUTE_FILE_LOCATION" +
+    " --feature-name $env:FEATURE_NAME" +
+    " --hosting-directory $env:HOSTING_DIRECTORY" +
+    " --primary-application-name $env:PRIMARY_APPLICATION_NAME" +
+    " --secondary-application-name $env:SECONDARY_APPLICATION_NAME"
+    )
+
 Push-Location $env:NOTEPAD_PLUS_PLUS_FILE_MANAGEMENT_EXECUTIVE_FILE_CONTAINING_DIRECTORY
-$ArgumentList = "--application $env:APPLICATION --command $env:COMMAND --notepaddpp-execute-file-location $env:NOTEPADDPP_EXECUTE_FILE_LOCATION --feature-name $env:FEATURE_NAME  --hosting-directory $env:HOSTING_DIRECTORY --primary-application-name $env:PRIMARY_APPLICATION_NAME --secondary-application-name $env:SECONDARY_APPLICATION_NAME"
+
 start-process -FilePath $env:NOTEPAD_PLUS_PLUS_FILE_MANAGEMENT_EXECUTIVE_FILE_LOCATION -ArgumentList $ArgumentList
+
 Pop-Location
